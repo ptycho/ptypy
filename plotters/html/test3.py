808a47cf94af403fe76999c1c508dd3b5f285861
Pierre Thibault <p.thibault@ucl.ac.uk>
2015-01-22 13:58:07 +0000

    Trivial PEP8 reformatting

--- plotters/html/test3.py
+++ plotters/html/test3.py
@@ -7,35 +7,36 @@ import webbrowser
 from wsgiref.simple_server import make_server
 import random
 
-#Choose the html/javascript frontend we will be working with
-FILE = 'Desktop/new_server/frontend2.html'
-#Choose the local port to use
+# Choose the html/javascript frontend we will be working with
+FILE = 'frontend.html'
+# Choose the local port to use
 PORT = 8080
 
-#Define a class to randomly select a stored image
+# Define a class to randomly select a stored image
 def get_image_file():
     
-    #Define an array of stored images
+    # Define an array of stored images
     svg_file_list = ['Desktop/new_server/x1.svg', 'Desktop/new_server/x2.svg', 'Desktop/new_server/x3.svg', 'Desktop/new_server/x4.svg']
-    #Choose one at random
+    # Choose one at random
     svg_file = random.choice(svg_file_list)
     return svg_file
 
+
 def test_app(environ, start_response):
-    if environ['REQUEST_METHOD'] == 'POST':                 #If we recieve a "POST" request from the webpage 
-        try:                                                #(i.e an interactive request, not from the URL)
-            response_body = file(get_image_file()).read()   #set our response as the desired image
+    if environ['REQUEST_METHOD'] == 'POST':                 # If we receive a "POST" request from the webpage
+        try:                                                # (i.e an interactive request, not from the URL)
+            response_body = file(get_image_file()).read()   # set our response as the desired image
       
         except:
             response_body = "An error has occurred" 
             
-        #Define the type of content being sent to the browswer and return it                            
+        # Define the type of content being sent to the browser and return it
         status = '200 OK'
         headers = [('Content-type', 'image/svg+xml'), ('Content-Length', str(len(response_body)))]
         start_response(status, headers)
         return [response_body]    
     
-    #If we recieve any other type of request such as a "GET" request, re-open the html file.
+    # If we receive any other type of request such as a "GET" request, re-open the html file.
     else:
         response_body = open(FILE).read()
     headers = [('Content-type', 'text/html'), ('Content-Length', str(len(response_body)))]
@@ -43,15 +44,17 @@ def test_app(environ, start_response):
     start_response(status, headers)
     return [response_body]
 
+
 def open_browser():
-    #Start a browser after waiting for half a second.
+    # Start a browser after waiting for half a second.
     def _open_browser():
         webbrowser.open('http://localhost:%s/%s' % (PORT, FILE))
     thread = threading.Timer(0.5, _open_browser)
     thread.start()
 
+
 def start_server():
-    #Start the server
+    # Start the server
     httpd = make_server("", PORT, test_app)
     httpd.serve_forever()
 
