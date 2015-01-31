a4f5ffa786ce786926a459b98ee628142f6da025
Pierre Thibault <p.thibault@ucl.ac.uk>
2015-01-31 13:56:55 +0000

    Bug fixes in I13.py

--- ptypy/experiment/I13.py
+++ ptypy/experiment/I13.py
@@ -19,8 +19,8 @@ from ..core.data import PtyScan
 logger = u.verbose.logger
 
 NEXUS_PATHS = u.Param()
-NEXUS_PATHS.frame = 'entry1/instrument/{detector_name}/data'
-NEXUS_PATHS.exposure = 'entry1/instrument/{detector_name}/count_time'
+NEXUS_PATHS.frame_pattern = 'entry1/instrument/%(detector_name)s/data'
+NEXUS_PATHS.exposure = 'entry1/instrument/%(detector_name)s/count_time'
 NEXUS_PATHS.motors = 'entry1/instrument/lab_sxy'
 NEXUS_PATHS.command = 'entry1/scan_command'
 NEXUS_PATHS.label = 'entry1/entry_identifier'
@@ -29,8 +29,8 @@ NEXUS_PATHS.experiment = 'entry1/experiment_identifier'
 DEFAULT = u.Param()
 DEFAULT.experimentID = None   # Experiment identifier
 DEFAULT.scan_number = 74      # scan number
-DEFAULT.dark_number = 72
-DEFAULT.flat_number = 72
+DEFAULT.dark_number = None
+DEFAULT.flat_number = None
 DEFAULT.energy = 9.7
 DEFAULT.lam = 1.2398e-9 / DEFAULT.energy
 DEFAULT.z = 7.5                                          # Distance from object to screen
@@ -40,9 +40,9 @@ DEFAULT.orientation = (True, True, False)
 DEFAULT.experimentID = None
 # IO
 DEFAULT.base_path = './'
-DEFAULT.data_dir_pattern = '%(base_path)s' + 'raw/%(scan_number)05d.nxs'
-DEFAULT.dark_dir_pattern = '%(base_path)s' + 'raw/%(dark_number)05d.nxs'
-DEFAULT.flat_dir_pattern = '%(base_path)s' + 'raw/%(flat_number)05d.nxs'
+DEFAULT.data_file_pattern = '%(base_path)s' + 'raw/%(scan_number)05d.nxs'
+DEFAULT.dark_file_pattern = '%(base_path)s' + 'raw/%(dark_number)05d.nxs'
+DEFAULT.flat_file_pattern = '%(base_path)s' + 'raw/%(flat_number)05d.nxs'
 DEFAULT.mask_file = '%(base_path)s' + 'processing/mask.h5'
 DEFAULT.motors = ['lab_sy', 'lab_sx']     # 'Motor names to determine the sample translation'
 DEFAULT.motors_multiplier = 1e-6       # 'Motor conversion factor to meters'
@@ -69,20 +69,7 @@ class I13Scan(PtyScan):
             p.update(pars)
         super(I13Scan, self).__init__(p, **kwargs)
 
-        self._autofill()
-
-        logger.info(u.verbose.report(self.info))
-
-        # self.info has been initialized by the parent class
-        self.info.data_file = self.info.data_file_pattern % self.info
-        self.info.data_file = None if self.info.dark_number is None else self.info.dark_file_pattern % self.info
-        self.info.flat_file = None if self.info.flat_number is None else self.info.flat_file_pattern % self.info
-
-    def _autofill(self):
-        """
-        Attempt to extract missing information about the experiment.
-        """
-        # Base path
+        # Attempt to extract base_path if missing
         if self.info.base_path is None:
             d = os.getcwd()
             base_path = None
@@ -98,14 +85,20 @@ class I13Scan(PtyScan):
             else:
                 self.info.base_path = base_path
 
-        # Experiment ID
+        self.info.data_file = self.info.data_file_pattern % self.info
+        self.info.dark_file = None if self.info.dark_number is None else self.info.dark_file_pattern % self.info
+        self.info.flat_file = None if self.info.flat_number is None else self.info.flat_file_pattern % self.info
+
+        # Attempt to extract experiment ID
         if self.info.experimentID is None:
             try:
                 experimentID = io.h5read(self.info.data_file, NEXUS_PATHS.experiment)[NEXUS_PATHS.experiment]
             except:
-                logger.debug('Could not find experiment ID from nexus file %s.' % self.scan)
+                logger.debug('Could not find experiment ID from nexus file %s.' % self.info.data_file)
                 experimentID = os.path.split(self.info.base_path[:-1])[1]
             self.info.experimentID = experimentID
+
+        logger.info(u.verbose.report(self.info))
 
     def load_common(self):
         """
@@ -121,7 +114,8 @@ class I13Scan(PtyScan):
 
         # FIXME: do something better here. (detector-dependent)
         # Load mask
-        common.weight2d = io.h5read('self.info.mask_file', 'mask')['mask'].astype(float)
+        if self.info.mask_file is not None:
+            common.weight2d = io.h5read('self.info.mask_file', 'mask')['mask'].astype(float)
 
         return common._to_dict()
         
@@ -152,7 +146,8 @@ class I13Scan(PtyScan):
         pos = {}
         weights = {}
         for j in indices:
-            raw[j] = io.h5read(self.info.data_file, NEXUS_PATHS.frame, slice=slice(j))
+            key = NEXUS_PATHS.frame_pattern % self.info
+            raw[j] = io.h5read(self.info.data_file, NEXUS_PATHS.frame_pattern % self.info, slice=j)[key]
             
         return raw, pos, weights
         
