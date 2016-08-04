83ed81733033a2c63f1fbfc9fd1285ee8ff9eebe
Stefanos Chalkidis <stefanos-horst.chalkidis.13@ucl.ac.uk>
2016-08-04 14:50:47 +0100

    Indentation error & removal print statement

--- ptypy/utils/test_utils.py
+++ ptypy/utils/test_utils.py
@@ -1,8 +1,13 @@
-'''
-some utilities to help with writing tests
-'''
+# -*- coding: utf-8 -*-
+"""\
+Some utilities to help with writing tests.
 
+This file is part of the PTYPY package.
 
-    path = inspect.stack()[0][1]
-    return '/'.join(os.path.split(path)[0].split(os.sep)[:-2] +
-                    ['test_data/process_lists', name])
+    :copyright: Copyright 2014 by the PTYPY team, see AUTHORS.
+    :license: GPLv2, see LICENSE for details.
+"""
+    # SC: indentation error, commented out for now
+    #path = inspect.stack()[0][1]
+    #return '/'.join(os.path.split(path)[0].split(os.sep)[:-2] +
+    #                ['test_data/process_lists', name])
