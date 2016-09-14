d6f175a6e89e1edb304fd6b7007deaedbd615d53
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2016-09-14 12:24:26 +0100

    Testing coordinate system

--- ptypy/experiment/AMO_LCLS.py
+++ ptypy/experiment/AMO_LCLS.py
@@ -135,7 +135,7 @@ class AMOScan(core.data.PtyScan):
 
         pos_list = []
         for i in range(0,len(x),self.info.recipe.averaging_number):
-            pos_list.append([x[i],y[i]])
+            pos_list.append([-y[i],-x[i]])
         positions = np.array(pos_list)
 
         return positions
