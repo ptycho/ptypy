800af8a652ced8f1ac64ee79ae7a9c46ccb0b4cf
simone <simone.sala.2014@gmail.com>
2016-12-12 12:09:13 +0000

    FERMI_SS: working on storage of OPR modes and coeffs in dumps (problems with MPI?)

--- ptypy/engines/MLOPR.py
+++ ptypy/engines/MLOPR.py
@@ -246,9 +246,9 @@ class MLOPR(BaseEngine):
         logger.info('  ....  in coefficient calculation: %.2f' % tc)
 
         ### Storing OPR modes and coeffs in dumps => will it work in MPI ??
-        if self.p.autosave is not None and self.p.autosave.interval > 1:
-            if engine.curiter % self.p.autosave.interval == 0:
-                if self.p.autosave.store_OPR_iter:
+        if self.p.io.autosave is not None and self.p.io.autosave.interval > 1:
+            if engine.curiter % self.p.io.autosave.interval == 0:
+                if self.p.io.autosave.store_OPR_iter:
                     self.ptycho.runtime['OPR_modes'] = self.OPR_modes
                     self.ptycho.runtime['OPR_coeffs'] = self.OPR_coeffs
 
