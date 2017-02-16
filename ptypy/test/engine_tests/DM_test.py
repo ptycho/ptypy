c6bfef062821d4b3ebd7f657f07385656adf1372
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-16 10:10:48 +0000

    added engine test functionality. Some fail and need to be fixed in future

---
+++ ptypy/test/engine_tests/DM_test.py
@@ -0,0 +1,43 @@
+"""
+Test for the DM_simple engine.
+
+This file is part of the PTYPY package.
+    :copyright: Copyright 2014 by the PTYPY team, see AUTHORS.
+    :license: GPLv2, see LICENSE for details.
+"""
+
+import unittest
+from ptypy.test import test_utils as tu 
+from ptypy import utils as u
+
+class DMTest(unittest.TestCase):
+    def test_DM_farfield(self):
+        engine_params = u.Param()
+        engine_params.name = 'DM'
+        engine_params.numiter = 5
+        engine_params.alpha =1
+        engine_params.probe_update_start = 2
+        engine_params.overlap_converge_factor = 0.05
+        engine_params.overlap_max_iterations = 10
+        engine_params.probe_inertia = 1e-3
+        engine_params.object_inertia = 0.1
+        engine_params.fourier_relax_factor = 0.01
+        engine_params.obj_smooth_std = 20
+        tu.EngineTestRunner(engine_params)
+        
+    def test_DM_nearfield(self):
+        engine_params = u.Param()
+        engine_params.name = 'DM'
+        engine_params.numiter = 5
+        engine_params.alpha =1
+        engine_params.probe_update_start = 2
+        engine_params.overlap_converge_factor = 0.05
+        engine_params.overlap_max_iterations = 10
+        engine_params.probe_inertia = 1e-3
+        engine_params.object_inertia = 0.1
+        engine_params.fourier_relax_factor = 0.01
+        engine_params.obj_smooth_std = 20
+        tu.EngineTestRunner(engine_params,propagator='nearfield')
+
+if __name__ == "__main__":
+    unittest.main()
--- ptypy/test/test_utils.py
+++ ptypy/test/test_utils.py
@@ -13,6 +13,8 @@ import inspect
 import os
 import tempfile
 from .. import utils as u
+from ptypy.core import Ptycho
+
 
 def get_test_data_path(name):
     path = inspect.stack()[0][1]
@@ -37,3 +39,50 @@ def PtyscanTestRunner(ptyscan_instance,r=u.Param(),data=u.Param(),save_type='app
             i+=1
         return out_dict
 
+def EngineTestRunner(engine_params,propagator='farfield'):
+    
+    RECIPE = u.Param({'photons': 100000000.0,
+                      'psf': 0.0,
+                      'density': 0.2})
+#     outdir = tempfile.mkdtemp()
+    p = u.Param()
+    p.verbose_level = 3                              
+    p.io = u.Param()
+    p.io.home = './'
+    p.io.rfile = False
+    p.io.autosave = None
+    p.io.autoplot = None
+    p.ipython_kernel = False
+    p.scans = u.Param()
+    p.scans.MF = u.Param()
+    p.scans.MF.data= u.Param()
+    p.scans.MF.data.source = 'test'
+    p.scans.MF.data.positions_theory =  None
+    p.scans.MF.data.auto_center =  None
+    p.scans.MF.data.min_frames =  1
+    p.scans.MF.data.orientation =  None
+    p.scans.MF.data.precedence =  None
+    p.scans.MF.data.num_frames =  100
+    p.scans.MF.data.energy =  6.2
+    p.scans.MF.data.shape =  256 
+    p.scans.MF.data.chunk_format = '.chunk%02d'
+    p.scans.MF.data.rebin =  None 
+    p.scans.MF.data.experimentID =  None 
+    p.scans.MF.data.label =  None 
+    p.scans.MF.data.version = 0.1 
+    p.scans.MF.data.dfile =  None 
+    p.scans.MF.data.lam =  None 
+    p.scans.MF.data.psize =  0.000172 
+    p.scans.MF.data.load_parallel =  None
+    p.scans.MF.data.misfit =  0
+    p.scans.MF.data.origin = 'fftshift'
+    p.scans.MF.data.distance =  7.0
+    p.scans.MF.data.save =  None
+    p.scans.MF.data.center = 'fftshift'
+    p.scans.MF.data.propagation = propagator
+    p.scans.MF.data.resolution =  None
+    p.scans.MF.data.recipe =  RECIPE
+    p.engines = u.Param()                              
+    p.engines.engine00 = engine_params
+    P = Ptycho(p,level=5)
+    return P
