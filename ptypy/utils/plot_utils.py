4eb8ffef57f92623e6fe606e4e1541fd14a61c11
Bjoern Enders <benders@tum.de>
2017-08-29 11:52:15 -0700

    small addition to Alex' fix

--- ptypy/utils/plot_utils.py
+++ ptypy/utils/plot_utils.py
@@ -173,6 +173,7 @@ def complex2hsv(cin, vmin=0., vmax=None):
         vmax = v.max()
     if vmin==vmax:
         v = np.ones_like(v) * v.mean()
+        v = v.clip(0.0, 1.0)
     else:
         assert vmin < vmax
         v = (v.clip(vmin, vmax)-vmin)/(vmax-vmin)
