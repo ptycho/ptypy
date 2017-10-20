75f58b856402f7aeb04c4f31d919a56924b784d9
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-10-20 11:28:15 +0200

    Adapted UCLLaserScan - not tested

--- ptypy/experiment/__init__.py
+++ ptypy/experiment/__init__.py
@@ -28,7 +28,7 @@ This file is part of the PTYPY package.
 # from AMO_LCLS import AMOScan
 # from DiProI_FERMI import DiProIFERMIScan
 # from optiklabor import FliSpecScanMultexp
-# from UCL import UCLLaserScan
+from UCL import UCLLaserScan
 from nanomax import NanomaxStepscanMay2017, NanomaxStepscanNov2016, NanomaxFlyscanJune2017
 from ALS_5321 import ALS5321Scan
 
