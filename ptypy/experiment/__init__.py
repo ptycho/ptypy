8794aef33f42a252edc39fd099314828f3b60412
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-10-20 10:52:02 +0200

    Adapted ALS5321Scan - tested

--- ptypy/experiment/__init__.py
+++ ptypy/experiment/__init__.py
@@ -30,7 +30,7 @@ This file is part of the PTYPY package.
 # from optiklabor import FliSpecScanMultexp
 # from UCL import UCLLaserScan
 from nanomax import NanomaxStepscanMay2017, NanomaxStepscanNov2016, NanomaxFlyscanJune2017
-# from ALS_5321 import ALS5321Scan
+from ALS_5321 import ALS5321Scan
 
 
 if __name__ == "__main__":
