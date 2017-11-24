0815719ea3bb937b6b74d93648ed7ea78ad730ed
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-11-24 16:59:42 +0100

    WIP: templates make_sample_ptyd and minimal_load_and_run work

--- templates/make_sample_ptyd.py
+++ templates/make_sample_ptyd.py
@@ -6,13 +6,12 @@ import sys
 import time
 import ptypy
 from ptypy import utils as u
-from ptypy import experiment
+from ptypy.core.data import MoonFlowerScan
 # for verbose output
 u.verbose.set_level(3)
 
 # create data parameter branch
 data = u.Param()
-data.source = 'test'
 data.dfile = 'sample.ptyd'
 data.num_frames = 100
 data.save = 'append'
@@ -22,10 +21,7 @@ data.rebin = None
 data.orientation = None
 
 # create PtyScan instance
-# The following call is equivalent to
-# MF = ptypy.core.data.MoonFlowerScan(data)
-# It uses data.source to find the proper PtyScan subclass
-MF = experiment.makePtyScan(data)
+MF = MoonFlowerScan(data)
 
 MF.initialize()
 for i in range(2):
