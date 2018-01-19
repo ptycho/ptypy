77de29e637923b50e4a5280be322283e4cc9c29c
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-01-19 10:22:07 +0000

    refactor so that this now takes the arrays not a dictionary

--- ptypy/gpu/object_probe_interaction.py
+++ ptypy/gpu/object_probe_interaction.py
@@ -7,43 +7,32 @@ Should have all the engine updates
 
 import numpy as np
 
-def get_exit_wave(serialized_scan):
+def get_exit_wave(obj, probe, exit_wave, addr):
     '''
     probably useful for Darren
     '''
-    meta = serialized_scan['meta'] # probably want to extract these at a later date, but just to get stuff going...
-    probe = serialized_scan['probe']
-    obj = serialized_scan['obj']
-    corrected_farfield_stack = serialized_scan['exit wave']
     sh = probe.shape
     view_dlayer = 0 # what is this?
-    addr_info = meta['addr'][:,(view_dlayer)] # addresses, object references
+    addr_info = addr[:,(view_dlayer)] # addresses, object references
     for pa, oa, ea,  _da, _ma in addr_info:
 #         print("pa:%s, oa:%s, ea:%s, da:%s, ma:%s" % (pa, oa, ea,  da, ma))
-        corrected_farfield_stack[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])] = \
+        exit_wave[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])] = \
             np.multiply(probe[pa[0], pa[1]:(pa[1]+sh[1]), pa[2]:(pa[2]+sh[2])], 
                         obj[oa[0], oa[1]:(oa[1]+sh[1]), oa[2]:(oa[2]+sh[2])])
-    serialized_scan['exit wave'] = corrected_farfield_stack
-    return serialized_scan
+    return exit_wave
 
-def difference_map_realspace_constraint(serialized_scan, alpha):
+def difference_map_realspace_constraint(obj, probe, exit_wave, addr, alpha):
     '''
     in theory this can just be called in ptypy instead of get_exit_wave
     '''
-    meta = serialized_scan['meta'] # probably want to extract these at a later date, but just to get stuff going...
-    probe = serialized_scan['probe']
     sh = probe.shape
-
-    obj = serialized_scan['obj']
-    corrected_farfield_stack = serialized_scan['exit wave']
     view_dlayer = 0 # what is this?
-    addr_info = meta['addr'][:,(view_dlayer)] # addresses, object references
+    addr_info = addr[:,(view_dlayer)] # addresses, object references
     for pa, oa, ea,  _da, _ma in addr_info:
         foo = np.multiply(probe[pa[0], pa[1]:(pa[1]+sh[1]), pa[2]:(pa[2]+sh[2])], 
                           obj[oa[0], oa[1]:(oa[1]+sh[1]), oa[2]:(oa[2]+sh[2])])
-        corrected_farfield_stack[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])] = \
-        (1.0 + alpha) * foo - alpha * corrected_farfield_stack[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])]
-    serialized_scan['exit wave'] = corrected_farfield_stack
-    return serialized_scan
+        exit_wave[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])] = \
+        (1.0 + alpha) * foo - alpha * exit_wave[ea[0], ea[1]:(ea[1]+sh[1]), ea[2]:(ea[2]+sh[2])]
+    return exit_wave
 
 
--- ptypy/test/gpu_tests/object_probe_interaction_test.py
+++ ptypy/test/gpu_tests/object_probe_interaction_test.py
@@ -15,12 +15,19 @@ class ObjectProbeInteractionTest(unittest.TestCase):
         self.PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
         # now convert to arrays
         self.serialized_scan = du.pod_to_arrays(self.PtychoInstance, 'S0000')
-
+        self.addr = self.serialized_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
+        self.probe = self.serialized_scan['probe']
+        self.obj = self.serialized_scan['obj']
+        self.exit_wave = self.serialized_scan['exit wave']
     def test_get_exit_wave(self):
-        opi.get_exit_wave(self.serialized_scan)
+        opi.get_exit_wave(self.obj, self.probe, self.exit_wave, self.addr)
     
     def test_difference_map_realspace_constraint(self):
-        opi.difference_map_realspace_constraint(self.serialized_scan, alpha=1.0)
+        opi.difference_map_realspace_constraint(self.obj,
+                                                self.probe,
+                                                self.exit_wave,
+                                                self.addr,
+                                                alpha=1.0)
 
 if __name__ == "__main__":
     unittest.main()
