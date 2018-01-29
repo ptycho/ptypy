c10228147a98590e7fc7d813d129e5e6eb4d7c4d
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-01-29 13:58:34 +0000

    checks the unity between scipy use case of ffts

--- ptypy/gpu/propagation.py
+++ ptypy/gpu/propagation.py
@@ -2,7 +2,8 @@
 All propagation based kernels 
 '''
 import numpy as np
-from object_probe_interaction import difference_map_realspace_constraint
+import scipy as sci
+from object_probe_interaction import difference_map_realspace_constraint, scan_and_multiply
 
 def farfield_propagator(data_to_be_transformed, prefilter=None, postfilter=None, direction='forward'):
     '''
@@ -14,30 +15,45 @@ def farfield_propagator(data_to_be_transformed, prefilter=None, postfilter=None,
     :param direction. The direction of the transform forward or backward. Default: Forward.
     :return: The transformed stack.
     '''
-    dt = data_to_be_transformed.dtype
+
     if direction is 'forward':
-        fft = np.fft.fft2
+        # fft = np.fft.fft2
+        fft = sci.fftpack.fft2
         sc = 1.0 / np.sqrt(np.prod(data_to_be_transformed.shape[-2:]))
 
     elif direction is 'backward':
-        fft  = np.fft.ifft2
+        # fft  = np.fft.ifft2
+        fft = sci.fftpack.ifft2
         sc = np.sqrt(np.prod(data_to_be_transformed.shape[-2:]))
     
     if (prefilter is None) and (postfilter is None):
-        
-        return fft(data_to_be_transformed, axes=(-2,-1)).astype(dt) * sc
+        return fft(data_to_be_transformed, axes=(-2,-1)) * sc
     elif (prefilter is None) and (postfilter is not None):
-        return np.multiply(postfilter.astype(dt), fft(data_to_be_transformed, axes=(-2,-1)).astype(dt)) * sc
+        return np.multiply(postfilter, fft(data_to_be_transformed, axes=(-2,-1))) * sc
     elif (prefilter is not None) and (postfilter is None):
-        return fft(np.multiply(data_to_be_transformed, prefilter.astype(dt)), axes=(-2,-1)).astype(dt) * sc
+        return fft(np.multiply(data_to_be_transformed, prefilter), axes=(-2,-1))* sc
     elif (prefilter is not None) and (postfilter is not None):
-        return np.multiply(postfilter.astype(dt), fft(np.multiply(data_to_be_transformed, prefilter.astype(dt)), axes=(-2,-1)).astype(dt)) * sc
+        return np.multiply(postfilter, fft(np.multiply(data_to_be_transformed, prefilter), axes=(-2,-1))) * sc
 
 def sqrt_abs(diffraction):
     return np.sqrt(np.abs(diffraction))
 
 
-def difference_map_fourier_constraint(mask, Idata, obj, probe, exit_wave, addr, prefilter, postfilter, pbound=None, alpha=1, LL_error=True):
+
+
+
+def calculate_err_phot(Idata, LL, addr_info, mask):
+    err_phot = np.zeros(Idata.shape[0])
+    for _pa, _oa, _ea, da, ma in addr_info:
+        # LLminI2 = np.power((LL[da[0]] - Idata[da[0]]), 2)
+        # timessum = np.sum(np.divide(np.multiply(mask[ma[0]], LLminI2), np.add(Idata[da[0]], 1.0)))
+        # err_phot[da[0]] = np.divide(timessum, np.prod(LL[da[0]].shape))
+        err_phot[da] = (np.sum(mask[ma] * (LL[da] - Idata[da])**2 / (Idata[da] + 1.))
+                    / np.prod(LL[da].shape))
+    return err_phot
+
+
+def difference_map_fourier_constraint(mask, Idata, obj, probe, exit_wave, addr, prefilter, postfilter, pbound=None, alpha=1.0, LL_error=True):
     '''
     This kernel just performs the fourier renormalisation.
     :param mask. The nd mask array
@@ -54,61 +70,68 @@ def difference_map_fourier_constraint(mask, Idata, obj, probe, exit_wave, addr, 
     sh = Idata.shape
     # For log likelihood error # need to double check this adp
     if LL_error is True:
-        LL = np.zeros_like(exit_wave)
-        po = np.zeros((sh[0], sh[1], sh[2]))
-        err_phot = np.zeros(sh[0])
-        for pa, oa, ea, _da, _ma in addr_info:
-            po[ea] = np.multiply(probe[pa[0],pa[1]:(pa[1]+sh[1]), pa[2]:(pa[2]+sh[2])],
-                                 obj[oa[0], oa[1]:(oa[1]+sh[1]), oa[2]:(oa[2]+sh[2])])
-
-        pof = np.power(np.abs(farfield_propagator(po, prefilter, postfilter, direction='forward')), 2)
-
+        LL = np.zeros_like(Idata, dtype=Idata.dtype)
+        po = scan_and_multiply(probe, obj, sh, addr_info)
+        fing = farfield_propagator(po[0], prefilter, postfilter, direction='forward')
+        pof = (fing * fing.conj()).real
+        import pylab as plt
+        plt.ion()
+        plt.figure(1)
+        plt.imshow(np.abs(po[10]))
+        plt.colorbar()
+        plt.show()
+        plt.savefig('/tmp/gpu_po.png')
         for _pa, _oa, ea, da, _ma in addr_info:
+            # print da[0], ea[0]
             LL[da[0]] += pof[ea[0]]
 
-        for _pa, _oa, _ea, da, ma in addr_info:
-            LLminI2 = np.power((LL[da[0]]- Idata[da[0]]), 2)
-            timessum = np.sum(np.divide(np.multiply(mask[ma[0]], LLminI2), np.add(Idata[da[0]], 1.0)))
-            err_phot[da[0]] = np.divide(timessum, np.prod(LL[da[0]].shape))
+        err_phot = calculate_err_phot(Idata, LL, addr_info, mask)
     else:
         err_phot = np.zeros(sh[0])
+    return err_phot
+    # # Propagate the exit waves
+    # constrained = difference_map_realspace_constraint(obj, probe, exit_wave, addr, alpha)
+    # f = farfield_propagator(constrained, prefilter, postfilter, direction='forward')
+    # for _pa, _oa, ea, da, _ma in addr_info:
+    #     af2[da[0]] += np.power(np.abs(f[ea[0]]), 2)
+    #
+    # fmag = np.sqrt(np.abs(Idata))
+    # af = np.sqrt(af2)
+    #
+    # # Fourier magnitudes deviations
+    # fdev = np.subtract(af, fmag)
+    # err_fmag = np.sum(np.multiply(mask, np.power(fdev, 2))) / np.sum(mask, axis=(0, 1))
+    # err_exit = 0.
+    #
+    # probe_object = scan_and_multiply(probe, obj, sh, addr_info)
+    # fm = None
+    # for _pa, _oa, ea, da, ma in addr_info:
+    #     if (pbound is None) or (err_fmag[da[0]] > pbound[da[0]]):
+    #         # No power bound
+    #         if pbound is None:
+    #             fm = np.zeros_like(exit_wave)
+    #             fm[ea] = (1 - mask[ma[0]]) + mask[ma[0]] * fmag[da[0]] / (af[da[0]] + 1e-10)
+    #         elif err_fmag[da] > pbound[da]:
+    #             # Power bound is applied
+    #             fm = np.zeros_like(exit_wave)
+    #             fm[ea] = (1 - mask[ma[0]]) + mask[ma[0]] * (fmag[da[0]] + fdev[da[0]] * np.sqrt(pbound[da[0]] / err_fmag[da[0]])) / (af[da[0]] + 1e-10)
+    #
+    #
+    # if fm is None:
+    #     df = np.multiply(alpha, (np.subtract(probe_object, exit_wave)))
+    # else:
+    #     backpropagated_solution = farfield_propagator(np.multiply(fm, f), prefilter, postfilter, direction='backward')
+    #     df = np.subtract(backpropagated_solution, probe_object)
+    #
+    # exit_wave += df
+    # err_exit += np.mean(np.power(np.abs(df), 2))
+    #
+    # if pbound is not None:
+    #     # rescale the fmagnitude error to some meaning !!!
+    #     # PT: I am not sure I agree with this.
+    #     err_fmag /= pbound
+    #
+    # return exit_wave, np.array([err_fmag, err_phot, err_exit])
 
-    # Propagate the exit waves
-    constrained = difference_map_realspace_constraint(obj, probe, exit_wave, addr, alpha)
-    f = farfield_propagator(constrained, prefilter, postfilter, direction='forward')
-    for _pa, _oa, ea, da, _ma in addr_info:
-        af2[da[0]] += np.power(np.abs(f[ea[0]]), 2)
 
-    fmag = np.sqrt(np.abs(Idata))
-    af = np.sqrt(af2)
 
-    # Fourier magnitudes deviations
-    fdev = np.subtract(af, fmag)
-    err_fmag = np.sum(np.multiply(mask, np.power(fdev, 2))) / np.sum(mask, axis=(0, 1))
-    err_exit = 0.
-
-    df = np.zeros_like(exit_wave)
-
-    for _pa, _oa, ea, da, ma in addr_info:
-        if (pbound is None) or (err_fmag[da[0]] > pbound[da[0]]):
-            # No power bound
-            if pbound is None:
-                fm = (1 - mask[ma[0]]) + mask[ma[0]] * fmag[da[0]] / (af[da[0]] + 1e-10)
-
-            elif err_fmag[da] > pbound[da]:
-            # Power bound is applied
-                fm = (1 - mask[ma[0]]) + mask[ma[0]] * (fmag[da[0]] + fdev[da[0]] * np.sqrt(pbound[da[0]] / err_fmag[da[0]])) / (af[da[0]] + 1e-10)
-            df[ea[0]] += farfield_propagator(fm * f[ea[0]], prefilter, postfilter, direction='backward') - np.multiply(probe[pa[0], pa[1]:(pa[1]+sh[1]), pa[2]:(pa[2]+sh[2])], obj[oa[0], oa[1]:(oa[1]+sh[1]), oa[2]:(oa[2]+sh[2])])
-        else:
-            # Within power bound so no constraint applied.
-            df[ea[0]] = alpha * (np.multiply(probe[pa[0], pa[1]:(pa[1]+sh[1]), pa[2]:(pa[2]+sh[2])], obj[oa[0], oa[1]:(oa[1]+sh[1]), oa[2]:(oa[2]+sh[2])]) - exit_wave[ea[0]])
-
-    exit_wave += df
-    err_exit += np.mean(np.power(np.abs(df), 2))
-
-    if pbound is not None:
-        # rescale the fmagnitude error to some meaning !!!
-        # PT: I am not sure I agree with this.
-        err_fmag /= pbound
-
-    return exit_wave, np.array([err_fmag, err_phot, err_exit])
--- ptypy/test/gpu_tests/farfield_propagator_test.py
+++ ptypy/test/gpu_tests/farfield_propagator_test.py
@@ -8,141 +8,191 @@ import utils as tu
 from ptypy.gpu import data_utils as du
 from ptypy.gpu import object_probe_interaction as opi
 from ptypy.gpu import propagation as prop
-
+from copy import deepcopy as copy
 
 class FarfieldPropagatorTest(unittest.TestCase):
     def setUp(self):
         self.PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
-        # now convert to arrays
         self.serialized_scan = du.pod_to_arrays(self.PtychoInstance, 'S0000')
-        self.serialized_scan = opi.difference_map_realspace_constraint(self.serialized_scan, alpha=1.0)
-        first_view_id = self.serialized_scan['meta']['view_IDs'][0]
+        self.addr = self.serialized_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
+        self.probe = self.serialized_scan['probe']
+        self.obj = self.serialized_scan['obj']
         self.exit_wave = self.serialized_scan['exit wave']
-        self.master_pod = self.PtychoInstance.diff.V[first_view_id].pod
+        first_view_id = self.serialized_scan['meta']['view_IDs'][0]
+        master_pod = self.PtychoInstance.diff.V[first_view_id].pod
+        self.propagator = master_pod.geometry.propagator
+        addr_info = self.addr[:, 0]
+        self.exit_wave = opi.scan_and_multiply(self.probe, self.obj, self.exit_wave.shape, addr_info) # already tested
 
     def test_fourier_transform_farfield_nofilter(self):
         prop.farfield_propagator(self.exit_wave)
-#  
-#     def test_fourier_transform_farfield_nofilter_UNITY(self):
-#         propagator = self.master_pod.geometry.propagator
-#         propagator.pre_fft = np.ones(self.exit_wave['exit wave'].shape[-2:])
-#         propagator.post_fft = np.ones(self.exit_wave['exit wave'].shape[-2:])
-#         result_array_npy = prop.farfield_propagator(self.exit_wave, )
-#         result_array_geo = self.diffraction_transform_with_geo(propagator)
-#         self.assertTrue(np.allclose(result_array_npy, result_array_geo), atol=1e-5)
+
+    def test_fourier_transform_farfield_nofilter_UNITY(self):
+        pre_fft = 1.0
+        post_fft = 1.0
+        propagator = copy(self.propagator)
+        propagator.pre_fft = pre_fft
+        propagator.post_fft = post_fft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0 # what is this?
+        addr_info = meta['addr'][:,view_dlayer] # addresses, object references
+        for _pa, _oa, ea,  _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]])
+        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
 
     def test_fourier_transform_farfield_with_prefilter(self):
-        propagator = self.master_pod.geometry.propagator
-        prop.farfield_propagator(self.exit_wave, prefilter=propagator.pre_fft)
-   
-#     def test_fourier_transform_farfield_with_prefilter_UNITY(self):
-#         propagator = self.master_pod.geometry.propagator
-#         result_array_npy = prop.farfield_propagator(self.exit_wave,  prefilter=propagator.pre_fft)
-#         propagator.post_fft = np.ones(self.exit_wave['exit wave'].shape[-2:])
-#         result_array_geo = self.diffraction_transform_with_geo(propagator)
-#         dnp.plot.image(np.log10(np.abs(result_array_geo[0])), name='result geo')
-#         dnp.plot.image(np.log10(np.abs(result_array_npy[0])), name='result np')
-#         self.assertTrue(np.allclose(result_array_npy, result_array_geo))
-# #   
+        prop.farfield_propagator(self.exit_wave, prefilter=self.propagator.pre_fft)
+
+    def test_fourier_transform_farfield_with_prefilter_UNITY(self):
+        pre_fft = self.propagator.pre_fft
+        post_fft = 1.0  # np.ones_like(self.propagator.post_fft)
+        propagator = copy(self.propagator)
+        propagator.pre_fft = pre_fft
+        propagator.post_fft = post_fft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], prefilter=pre_fft)
+        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
+
     def test_fourier_transform_farfield_with_postfilter(self):
-        propagator = self.master_pod.geometry.propagator
-        prop.farfield_propagator(self.exit_wave,  postfilter=propagator.post_fft)
-#   
-#     def test_fourier_transform_farfield_with_postfilter_UNITY(self):
-#         geo = copy(self.master_geo)
-#         shape = self.exit_wave['probe'].shape[-2:]
-#         filters = prop.generate_far_field_fft_filters(geo.lam * geo.distance, shape, geo.resolution, geo.psize)
-#         result_array_npy = prop.farfield_propagator(self.exit_wave,  postfilter=filters[1])
-#         geo.pre_fft = np.ones_like(self.exit_wave['exit wave'].shape[-2:])
-#         result_array_geo = self.diffraction_transform_with_geo(geo)
-#         self.assertTrue(np.allclose(result_array_npy, result_array_geo))
-#   
+        prop.farfield_propagator(self.exit_wave,  postfilter=self.propagator.post_fft)
+
+    def test_fourier_transform_farfield_with_postfilter_UNITY(self):
+        pre_fft = 1.0
+        post_fft = self.propagator.post_fft
+        propagator = copy(self.propagator)
+        propagator.pre_fft = pre_fft
+        propagator.post_fft = post_fft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], postfilter=post_fft)
+        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
+
     def test_fourier_transform_farfield_with_pre_and_post_filter(self):
-        propagator = self.master_pod.geometry.propagator
-        prop.farfield_propagator(self.exit_wave, 
-                               prefilter=propagator.pre_fft,
-                               postfilter=propagator.post_fft)
-#   
-#     def test_fourier_transform_farfield_with_pre_and_post_filter_UNITY(self):
-#         geo = copy(self.master_geo)
-#         shape = self.exit_wave['probe'].shape[-2:]
-#         filters = prop.generate_far_field_fft_filters(geo.lam * geo.distance, shape, geo.resolution, geo.psize)
-#         result_array_npy = prop.farfield_propagator(self.exit_wave,  prefilter=filters[0], postfilter=filters[1])
-#         result_array_geo = self.diffraction_transform_with_geo(geo)
-#         self.assertTrue(np.allclose(result_array_npy, result_array_geo))
-#   
+        prop.farfield_propagator(self.exit_wave,
+                               prefilter=self.propagator.pre_fft,
+                               postfilter=self.propagator.post_fft)
+
+    def test_fourier_transform_farfield_with_pre_and_post_filter_UNITY(self):
+        pre_fft = self.propagator.pre_fft
+        post_fft = self.propagator.post_fft
+        propagator = copy(self.propagator)
+        propagator.pre_fft = pre_fft
+        propagator.post_fft = post_fft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], prefilter=pre_fft, postfilter=post_fft)
+        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
+
+
     def test_inverse_fourier_transform_farfield_nofilter(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                )
-        prop.farfield_propagator(farfield_stack,
-                               
-                               direction='backward')
-#         
-   
+        prop.farfield_propagator(self.exit_wave, direction='backward')
+
+    def test_inverse_fourier_transform_farfield_nofilter_UNITY(self):
+        pre_ifft = 1.0
+        post_ifft = 1.0
+        propagator = copy(self.propagator)
+        propagator.pre_ifft = pre_ifft
+        propagator.post_ifft = post_ifft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0 # what is this?
+        addr_info = meta['addr'][:,view_dlayer] # addresses, object references
+        for _pa, _oa, ea,  _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], direction='backward')
+        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
+
     def test_inverse_fourier_transform_farfield_with_prefilter(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                
-                                                prefilter=propagator.pre_fft)
-        prop.farfield_propagator(farfield_stack,
-                               
-                               prefilter=propagator.pre_ifft,
-                               direction='backward')
+        prop.farfield_propagator(self.exit_wave, prefilter=self.propagator.pre_ifft)
+
+    def test_inverse_fourier_transform_farfield_with_prefilter_UNITY(self):
+        pre_ifft = self.propagator.pre_ifft
+        post_ifft = 1.0  # np.ones_like(self.propagator.post_ifft)
+        propagator = copy(self.propagator)
+        propagator.pre_ifft = pre_ifft
+        propagator.post_ifft = post_ifft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], prefilter=pre_ifft, direction='backward')
+        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
 
     def test_inverse_fourier_transform_farfield_with_postfilter(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                
-                                                postfilter=propagator.post_fft)
-        prop.farfield_propagator(farfield_stack,
-                               
-                               postfilter=propagator.post_ifft,
-                               direction='backward')
-   
+        prop.farfield_propagator(self.exit_wave,  postfilter=self.propagator.post_ifft, direction='backward')
+
+    def test_inverse_fourier_transform_farfield_with_postfilter_UNITY(self):
+        pre_ifft = 1.0
+        post_ifft = self.propagator.post_ifft
+        propagator = copy(self.propagator)
+        propagator.pre_ifft = pre_ifft
+        propagator.post_ifft = post_ifft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], postfilter=post_ifft, direction='backward')
+        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
+
     def test_inverse_fourier_transform_farfield_with_pre_and_post_filter(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                
-                                                prefilter=propagator.pre_fft,
-                                                postfilter=propagator.post_fft)
+        prop.farfield_propagator(self.exit_wave,
+                               prefilter=self.propagator.pre_ifft,
+                               postfilter=self.propagator.post_ifft,
+                                direction='backward')
 
-        result = prop.farfield_propagator(farfield_stack,
-                                         
-                                        prefilter=propagator.pre_ifft,
-                                        postfilter=propagator.post_ifft,
-                                        direction='backward')
-       
-    def test_inverse_fourier_transform_farfield_with_pre_and_post_filter_self_consistency(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                
-                                                prefilter=propagator.pre_fft,
-                                                postfilter=propagator.post_fft)
-        result = prop.farfield_propagator(farfield_stack,
-                                         
-                                        prefilter=propagator.pre_ifft,
-                                        postfilter=propagator.post_ifft)
-        self.assertTrue(np.allclose(result, self.exit_wave,atol=1e-5))
+    def test_inverse_fourier_transform_farfield_with_pre_and_post_filter_UNITY(self):
+        pre_ifft = self.propagator.pre_ifft
+        post_ifft = self.propagator.post_ifft
+        propagator = copy(self.propagator)
+        propagator.pre_ifft = pre_ifft
+        propagator.post_ifft = post_ifft
+        result_array_npy = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta']  # probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0  # what is this?
+        addr_info = meta['addr'][:, view_dlayer]  # addresses, object references
+        for _pa, _oa, ea, _da, _ma in addr_info:
+            result_array_npy[ea[0]] = prop.farfield_propagator(self.exit_wave[ea[0]], prefilter=pre_ifft, postfilter=post_ifft, direction='backward')
+        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        np.testing.assert_allclose(result_array_npy, result_array_geo)
 
-    def test_inverse_fourier_transform_farfield_self_consistency(self):
-        propagator = self.master_pod.geometry.propagator
-        farfield_stack = prop.farfield_propagator(self.exit_wave,
-                                                )
-        result = prop.farfield_propagator(farfield_stack,
-                                        
-                                        direction='backward')
 
-        self.assertTrue(np.allclose(result, self.exit_wave, atol=1e-5)) # this tolerance is equivalent to the current scipy implementation. It comes from the filters
-        
+    def inverse_diffraction_transform_with_geo(self, propagator):
+        result_array_geo = np.zeros_like(self.exit_wave)
+        meta = self.serialized_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
+        view_dlayer = 0 # what is this?
+        addr_info = meta['addr'][:,view_dlayer] # addresses, object references
+        for _pa, _oa, ea,  _da, _ma in addr_info:
+            result_array_geo[ea[0]] = propagator.bw(self.exit_wave[ea[0]])
+        return result_array_geo
+
+
     def diffraction_transform_with_geo(self, propagator):
         result_array_geo = np.zeros_like(self.exit_wave)
-        meta = self.serialized_scan['meta'] # probably want to extract these at a later date, but just to get stuff going...
+        meta = self.serialized_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
         view_dlayer = 0 # what is this?
-        addr_info = meta['addr'][:,(view_dlayer)] # addresses, object references
+        addr_info = meta['addr'][:,view_dlayer] # addresses, object references
         for _pa, _oa, ea,  _da, _ma in addr_info:
-            result_array_geo[ea[0]] = propagator.bw(propagator.fw(self.exit_wave[ea[0]]))
+            result_array_geo[ea[0]] = propagator.fw(self.exit_wave[ea[0]])
         return result_array_geo
-  
+#
 if __name__ == "__main__":
     unittest.main()
