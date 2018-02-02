e2a6d1afc3ff4465e02a49073abbecd3d860d222
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-02-02 17:41:13 +0000

    this now passes for the right reasons

--- ptypy/test/gpu_tests/farfield_propagator_test.py
+++ ptypy/test/gpu_tests/farfield_propagator_test.py
@@ -13,167 +13,194 @@ from copy import deepcopy as copy
 class FarfieldPropagatorTest(unittest.TestCase):
     def setUp(self):
         self.PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
+        self.GeoPtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
         self.vectorised_scan = du.pod_to_arrays(self.PtychoInstance, 'S0000')
-        self.addr = self.vectorised_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
-        self.probe = self.vectorised_scan['probe']
-        self.obj = self.vectorised_scan['obj']
-        self.exit_wave = self.vectorised_scan['exit wave']
-        first_view_id = self.vectorised_scan['meta']['view_IDs'][0]
-        master_pod = self.PtychoInstance.diff.V[first_view_id].pod
-        self.propagator = master_pod.geometry.propagator
-        addr_info = self.addr[:, 0]
-        self.exit_wave = opi.scan_and_multiply(self.probe, self.obj, self.exit_wave.shape, addr_info) # already tested
+        self.pod_vectorised_scan = du.pod_to_arrays(self.GeoPtychoInstance, 'S0000')
+        self.first_view_id = self.pod_vectorised_scan['meta']['view_IDs'][0]
 
     def test_fourier_transform_farfield_nofilter(self):
-        prop.farfield_propagator(self.exit_wave)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        prop.farfield_propagator(vec_ew)
 
     def test_fourier_transform_farfield_nofilter_UNITY(self):
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+
         pre_fft = 1.0
         post_fft = 1.0
-        propagator = copy(self.propagator)
-        propagator.pre_fft = pre_fft
-        propagator.post_fft = post_fft
-        result_array_npy = prop.farfield_propagator(self.exit_wave)
-        result_array_geo = self.diffraction_transform_with_geo(propagator)
+
+        geo_propagator.pre_fft = pre_fft
+        geo_propagator.post_fft = post_fft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=None, postfilter=None)
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew)
         np.testing.assert_allclose(result_array_npy, result_array_geo)
+
 
     def test_fourier_transform_farfield_with_prefilter(self):
-        prop.farfield_propagator(self.exit_wave, prefilter=self.propagator.pre_fft)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=propagator.pre_fft)
 
     def test_fourier_transform_farfield_with_prefilter_UNITY(self):
-        pre_fft = self.propagator.pre_fft
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+
         post_fft = 1.0
-        propagator = copy(self.propagator)
-        propagator.pre_fft = pre_fft
-        propagator.post_fft = post_fft
-        result_array_npy = prop.farfield_propagator(self.exit_wave, prefilter=pre_fft)
-        result_array_geo = self.diffraction_transform_with_geo(propagator)
+
+        geo_propagator.post_fft = post_fft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=propagator.pre_fft, postfilter=None)
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew)
         np.testing.assert_allclose(result_array_npy, result_array_geo)
 
+
     def test_fourier_transform_farfield_with_postfilter(self):
-        prop.farfield_propagator(self.exit_wave,
-                                 postfilter=self.propagator.post_fft)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=None, postfilter=propagator.post_fft)
 
     def test_fourier_transform_farfield_with_postfilter_UNITY(self):
-        pre_fft = 1.0
-        post_fft = self.propagator.post_fft
-        propagator = copy(self.propagator)
-        propagator.pre_fft = pre_fft
-        propagator.post_fft = post_fft
-        result_array_npy= prop.farfield_propagator(self.exit_wave,
-                                                   postfilter=post_fft)
-        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
 
+        pre_fft = 1.0
+
+        geo_propagator.pre_fft = pre_fft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=None, postfilter=propagator.post_fft)
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew)
         np.testing.assert_allclose(result_array_npy, result_array_geo)
 
     def test_fourier_transform_farfield_with_pre_and_post_filter(self):
-        prop.farfield_propagator(self.exit_wave,
-                                prefilter=self.propagator.pre_fft,
-                                postfilter=self.propagator.post_fft)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=propagator.pre_fft, postfilter=propagator.post_fft)
 
     def test_fourier_transform_farfield_with_pre_and_post_filter_UNITY(self):
-        pre_fft = self.propagator.pre_fft
-        post_fft = self.propagator.post_fft
-        propagator = copy(self.propagator)
-        propagator.pre_fft = pre_fft
-        propagator.post_fft = post_fft
-        result_array_npy = prop.farfield_propagator(self.exit_wave,
-                                                    prefilter=pre_fft,
-                                                    postfilter=post_fft)
-        result_array_geo = self.diffraction_transform_with_geo(propagator)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
 
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=propagator.pre_fft, postfilter=propagator.post_fft)
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew)
         np.testing.assert_allclose(result_array_npy, result_array_geo)
-
 
     def test_inverse_fourier_transform_farfield_nofilter(self):
-        prop.farfield_propagator(self.exit_wave, direction='backward')
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        prop.farfield_propagator(vec_ew, direction='backward')
 
     def test_inverse_fourier_transform_farfield_nofilter_UNITY(self):
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+
         pre_ifft = 1.0
         post_ifft = 1.0
-        propagator = copy(self.propagator)
-        propagator.pre_ifft = pre_ifft
-        propagator.post_ifft = post_ifft
-        result_array_npy = prop.farfield_propagator(self.exit_wave,
-                                                    direction='backward')
-        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
 
+        geo_propagator.pre_ifft = pre_ifft
+        geo_propagator.post_ifft = post_ifft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=None, postfilter=None, direction='backward')
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew, direction='backward')
         np.testing.assert_allclose(result_array_npy, result_array_geo)
+
 
     def test_inverse_fourier_transform_farfield_with_prefilter(self):
-        prop.farfield_propagator(self.exit_wave,
-                                 prefilter=self.propagator.pre_ifft)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=propagator.pre_ifft, direction='backward')
 
     def test_inverse_fourier_transform_farfield_with_prefilter_UNITY(self):
-        pre_ifft = self.propagator.pre_ifft
-        post_ifft = 1.0
-        propagator = copy(self.propagator)
-        propagator.pre_ifft = pre_ifft
-        propagator.post_ifft = post_ifft
-        result_array_npy = prop.farfield_propagator(self.exit_wave,
-                                                    prefilter=pre_ifft,
-                                                    direction='backward')
-        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
 
+        post_ifft = 1.0
+
+        geo_propagator.post_ifft = post_ifft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=propagator.pre_ifft, postfilter=None, direction='backward')
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew, direction='backward')
         np.testing.assert_allclose(result_array_npy, result_array_geo)
 
+
     def test_inverse_fourier_transform_farfield_with_postfilter(self):
-        prop.farfield_propagator(self.exit_wave,
-                                 postfilter=self.propagator.post_ifft,
-                                 direction='backward')
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=None, postfilter=propagator.post_ifft, direction='backward')
 
     def test_inverse_fourier_transform_farfield_with_postfilter_UNITY(self):
-        pre_ifft = 1.0
-        post_ifft = self.propagator.post_ifft
-        propagator = copy(self.propagator)
-        propagator.pre_ifft = pre_ifft
-        propagator.post_ifft = post_ifft
-        result_array_npy = prop.farfield_propagator(self.exit_wave,
-                                                    postfilter=post_ifft,
-                                                    direction='backward')
-        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
 
+        pre_ifft = 1.0
+
+        geo_propagator.pre_ifft = pre_ifft
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=None, postfilter=propagator.post_ifft, direction='backward')
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew, direction='backward')
         np.testing.assert_allclose(result_array_npy, result_array_geo)
 
     def test_inverse_fourier_transform_farfield_with_pre_and_post_filter(self):
-        prop.farfield_propagator(self.exit_wave,
-                               prefilter=self.propagator.pre_ifft,
-                               postfilter=self.propagator.post_ifft,
-                                direction='backward')
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        prop.farfield_propagator(vec_ew, prefilter=propagator.pre_ifft, postfilter=propagator.post_ifft, direction='backward')
 
     def test_inverse_fourier_transform_farfield_with_pre_and_post_filter_UNITY(self):
-        pre_ifft = self.propagator.pre_ifft
-        post_ifft = self.propagator.post_ifft
-        propagator = copy(self.propagator)
-        propagator.pre_ifft = pre_ifft
-        propagator.post_ifft = post_ifft
-        result_array_npy = prop.farfield_propagator(self.exit_wave,
-                                                    prefilter=pre_ifft,
-                                                    postfilter=post_ifft,
-                                                    direction='backward')
-        result_array_geo = self.inverse_diffraction_transform_with_geo(propagator)
+        vec_ew = self.get_exit_wave(self.vectorised_scan)
+        pod_ew = self.get_exit_wave(self.pod_vectorised_scan)
+        geo_propagator = copy(self.GeoPtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
+        propagator = copy(self.PtychoInstance.di.V[self.first_view_id].pod.geometry.propagator)
 
+
+        result_array_npy = prop.farfield_propagator(vec_ew, prefilter=propagator.pre_ifft, postfilter=propagator.post_ifft, direction='backward')
+        result_array_geo = self.diffraction_transform_with_geo(geo_propagator, pod_ew, direction='backward')
         np.testing.assert_allclose(result_array_npy, result_array_geo)
 
 
-    def inverse_diffraction_transform_with_geo(self, propagator):
-        result_array_geo = np.zeros_like(self.exit_wave)
-        meta = self.vectorised_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
+    def get_exit_wave(self, a_vectorised_scan):
+        '''
+        a pretested method
+        :param a_vectorised_scan: A scan that has been vectorised. 
+        :return: the exit wave
+        '''
+        vec_addr_info = a_vectorised_scan['meta']['addr'][:, 0]
+        vec_probe = a_vectorised_scan['probe']
+        vec_obj = a_vectorised_scan['obj']
+        vec_ew = a_vectorised_scan['exit wave']
+        return opi.scan_and_multiply(vec_probe,
+                                     vec_obj,
+                                     vec_ew.shape,
+                                     vec_addr_info)
+
+
+    def diffraction_transform_with_geo(self, propagator, ew, direction='forward'):
+        result_array_geo = np.zeros_like(ew)
+        meta = self.pod_vectorised_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
         view_dlayer = 0 # what is this?
         addr_info = meta['addr'][:,view_dlayer] # addresses, object references
         for _pa, _oa, ea,  _da, _ma in addr_info:
-            result_array_geo[ea[0]] = propagator.bw(self.exit_wave[ea[0]])
+            if direction=='forward':
+                result_array_geo[ea[0]] = propagator.fw(ew[ea[0]])
+            else:
+                result_array_geo[ea[0]] = propagator.bw(ew[ea[0]])
         return result_array_geo
 
 
-    def diffraction_transform_with_geo(self, propagator):
-        result_array_geo = np.zeros_like(self.exit_wave)
-        meta = self.vectorised_scan['meta'] #  probably want to extract these at a later date, but just to get stuff going...
-        view_dlayer = 0 # what is this?
-        addr_info = meta['addr'][:,view_dlayer] # addresses, object references
-        for _pa, _oa, ea,  _da, _ma in addr_info:
-            result_array_geo[ea[0]] = propagator.fw(self.exit_wave[ea[0]])
-        return result_array_geo
 #
+
 if __name__ == "__main__":
     unittest.main()
