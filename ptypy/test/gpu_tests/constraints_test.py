5473935e3f032015e97400195246f0b21f281e7a
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-02-02 17:42:33 +0000

    refactors this to make more sense. The output dictionary is now scrambled in order so I should check that.

--- ptypy/test/gpu_tests/constraints_test.py
+++ ptypy/test/gpu_tests/constraints_test.py
@@ -13,51 +13,67 @@ from ptypy.engines.utils import basic_fourier_update
 from ptypy.gpu.constraints import difference_map_fourier_constraint
 
 class ConstraintsTest(unittest.TestCase):
-    def setUp(self):
-        self.PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
-        # now convert to arrays
-        self.vectorised_scan = du.pod_to_arrays(self.PtychoInstance, 'S0000')
-        self.addr = self.vectorised_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
-        self.probe = self.vectorised_scan['probe']
-        self.obj = self.vectorised_scan['obj']
-        self.exit_wave = self.vectorised_scan['exit wave']
-        self.diffraction = self.vectorised_scan['diffraction']
-        self.mask = self.vectorised_scan['mask']
-        view_names = self.PtychoInstance.diff.views.keys()
-        self.error_dict = OrderedDict.fromkeys(view_names)
-        first_view_id = self.vectorised_scan['meta']['view_IDs'][0]
-        master_pod = self.PtychoInstance.diff.V[first_view_id].pod
-        self.propagator = master_pod.geometry.propagator
 
     def test_difference_map_fourier_constraint(self):
-        difference_map_fourier_constraint(self.mask,
-                                          self.diffraction,
-                                          self.obj,
-                                          self.probe,
-                                          self.exit_wave,
-                                          self.addr,
-                                          prefilter=self.propagator.pre_fft,
-                                          postfilter=self.propagator.post_fft,
+        PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
+        # now convert to arrays
+        vectorised_scan = du.pod_to_arrays(PtychoInstance, 'S0000')
+        addr = vectorised_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
+        probe = vectorised_scan['probe']
+        obj = vectorised_scan['obj']
+        exit_wave = vectorised_scan['exit wave']
+        diffraction = vectorised_scan['diffraction']
+        mask = vectorised_scan['mask']
+        view_names = PtychoInstance.diff.views.keys()
+        first_view_id = vectorised_scan['meta']['view_IDs'][0]
+        master_pod = PtychoInstance.diff.V[first_view_id].pod
+        propagator = master_pod.geometry.propagator
+        difference_map_fourier_constraint(mask,
+                                          diffraction,
+                                          obj,
+                                          probe,
+                                          exit_wave,
+                                          addr,
+                                          prefilter=propagator.pre_fft,
+                                          postfilter=propagator.post_fft,
                                           pbound=None,
                                           alpha=1.0,
                                           LL_error=True)
 
 
     def test_difference_map_fourier_constraint_UNITY(self):
-        ptypy_ewf, ptypy_error= self.ptypy_difference_map_fourier_constraint()
-        exit_wave, errors = difference_map_fourier_constraint(self.mask,
-                                                              self.diffraction,
-                                                              self.obj,
-                                                              self.probe,
-                                                              self.exit_wave,
-                                                              self.addr,
-                                                              prefilter=self.propagator.pre_fft,
-                                                              postfilter=self.propagator.post_fft,
+        PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
+        PodPtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
+
+        # now convert to arrays
+        vectorised_scan = du.pod_to_arrays(PtychoInstance, 'S0000')
+        addr = vectorised_scan['meta']['addr'] # probably want to extract these at a later date, but just to get stuff going...
+        probe = vectorised_scan['probe']
+        obj = vectorised_scan['obj']
+        exit_wave = vectorised_scan['exit wave']
+        diffraction = vectorised_scan['diffraction']
+        mask = vectorised_scan['mask']
+        view_names = PtychoInstance.diff.views.keys()
+
+        error_dict = OrderedDict.fromkeys(view_names)
+        first_view_id = vectorised_scan['meta']['view_IDs'][0]
+        master_pod = PtychoInstance.diff.V[first_view_id].pod
+        propagator = master_pod.geometry.propagator
+        ptypy_ewf, ptypy_error= self.ptypy_difference_map_fourier_constraint(PodPtychoInstance)
+        exit_wave, errors = difference_map_fourier_constraint(mask,
+                                                              diffraction,
+                                                              obj,
+                                                              probe,
+                                                              exit_wave,
+                                                              addr,
+                                                              prefilter=propagator.pre_fft,
+                                                              postfilter=propagator.post_fft,
                                                               pbound=None,
                                                               alpha=1.0,
                                                               LL_error=True)
 
         for idx, key in enumerate(ptypy_ewf.keys()):
+            print key, idx
             np.testing.assert_allclose(ptypy_ewf[key], exit_wave[idx])
 
         ptypy_fmag = []
@@ -77,29 +93,30 @@ class ConstraintsTest(unittest.TestCase):
         npy_fmag = errors[0, :]
         npy_phot = errors[1, :]
         npy_exit = errors[2, :]
+        import pylab as plt
+        x = np.arange(92)
+        plt.figure('fmag')
+        plt.plot(x, npy_fmag, x, ptypy_fmag)
+        plt.legend(['npy', 'ptypy'])
+        plt.show()
+        np.testing.assert_array_equal(npy_fmag, ptypy_fmag)
+
+        np.testing.assert_array_equal(npy_phot, ptypy_phot)
+        np.testing.assert_array_equal(npy_exit, ptypy_exit)
 
 
 
-        # import pylab as plt
-        # x = np.arange(92)
-        # plt.figure('fmag')
-        # plt.plot(x, npy_fmag, x, ptypy_fmag)
-        # plt.legend(['npy', 'ptypy'])
-        # plt.show()
 
 
 
 
 
 
-
-    def ptypy_difference_map_fourier_constraint(self):
-        ptycho_instance = copy(self.PtychoInstance)
+    def ptypy_difference_map_fourier_constraint(self, a_ptycho_instance):
         error_dct = OrderedDict()
         exit_wave = OrderedDict()
-
-        for dname in self.error_dict.keys():
-            di_view = ptycho_instance.diff.V[dname]
+        for dname, diff_view in a_ptycho_instance.diff.views.iteritems():
+            di_view = a_ptycho_instance.diff.V[dname]
             error_dct[dname] = basic_fourier_update(di_view,
                                                    pbound=None,
                                                    alpha=1.0)
