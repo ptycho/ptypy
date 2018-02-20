eec17e341bc1fd6fd57b69e2c1f5e6781e0d389b
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-02-20 09:53:45 +0000

    moved the casting of type in the propagators

--- ptypy/gpu/propagation.py
+++ ptypy/gpu/propagation.py
@@ -17,25 +17,26 @@ def farfield_propagator(data_to_be_transformed, prefilter=None, postfilter=None,
 
     if direction is 'forward':
         fft = lambda x: np.fft.fft2(x, axes=(-2, -1)).astype(COMPLEX_TYPE)
-        prefilter = prefilter.astype(COMPLEX_TYPE)
-        postfilter = postfilter.astype(COMPLEX_TYPE)
         # fft = sci.fftpack.fft2
         sc = 1.0 / np.sqrt(np.prod(data_to_be_transformed.shape[-2:]))
 
     elif direction is 'backward':
         fft = lambda x: np.fft.ifft2(x,  axes=(-2, -1)).astype(COMPLEX_TYPE)
         # fft = sci.fftpack.ifft2
-        prefilter = prefilter.astype(COMPLEX_TYPE)
-        postfilter = postfilter.astype(COMPLEX_TYPE)
+
         sc = np.sqrt(np.prod(data_to_be_transformed.shape[-2:]))
 
     if (prefilter is None) and (postfilter is None):
         return fft(data_to_be_transformed) * sc
     elif (prefilter is None) and (postfilter is not None):
+        postfilter = postfilter.astype(COMPLEX_TYPE)
         return np.multiply(postfilter, fft(data_to_be_transformed)) * sc
     elif (prefilter is not None) and (postfilter is None):
+        prefilter = prefilter.astype(COMPLEX_TYPE)
         return fft(np.multiply(data_to_be_transformed, prefilter))* sc
     elif (prefilter is not None) and (postfilter is not None):
+        prefilter = prefilter.astype(COMPLEX_TYPE)
+        postfilter = postfilter.astype(COMPLEX_TYPE)
         return np.multiply(postfilter, fft(np.multiply(data_to_be_transformed, prefilter))) * sc
 
 def sqrt_abs(diffraction):
