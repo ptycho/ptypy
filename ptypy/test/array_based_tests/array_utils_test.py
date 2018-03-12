ce8b3eb781f560a0fe70fed34ac7e720e4dbac72
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-03-12 09:42:00 +0000

    Make this test more explicit for joerg

--- ptypy/test/array_based_tests/array_utils_test.py
+++ ptypy/test/array_based_tests/array_utils_test.py
@@ -5,7 +5,7 @@ Tests for the array_utils module
 
 import unittest
 import numpy as np
-from ptypy.array_based import FLOAT_TYPE
+from ptypy.array_based import FLOAT_TYPE, COMPLEX_TYPE
 from ptypy.array_based import array_utils as au
 
 
@@ -37,20 +37,32 @@ class ArrayUtilsTest(unittest.TestCase):
         self.assertEqual(absed.dtype, np.float)
 
     def test_sum_to_buffer(self):
-        in1 = np.array([np.ones((4,4)),
-                        np.ones((4, 4))*2.0,
-                        np.ones((4, 4))*3.0,
-                        np.ones((4, 4))*4.0])
 
-        outshape = (2, 4, 4)
-        expected_out = np.array([np.ones((4, 4))*4.0,
-                                 np.ones((4, 4)) * 6.0])
+        I = 4
+        X = 2
+        M = 4
+        N = 4
+
+        in1 = np.empty((I, M, N), dtype=FLOAT_TYPE)
+
+        # fill the input array
+        for idx in range(I):
+            in1[idx] = np.ones((M, N))* (idx + 1.0)
+
+        outshape = (X, M, N)
+        expected_out = np.empty(outshape)
+
+        expected_out[0] = np.ones((M, N)) * 4.0
+        expected_out[1] = np.ones((M, N)) * 6.0
+
+        in1_addr = np.empty((I, 3))
 
         in1_addr = np.array([(0, 0, 0),
                             (1, 0, 0),
                             (2, 0, 0),
                             (3, 0, 0)])
 
+        out1_addr = np.empty_like(in1_addr)
         out1_addr = np.array([(0, 0, 0),
                               (1, 0, 0),
                               (0, 0, 0),
@@ -59,6 +71,42 @@ class ArrayUtilsTest(unittest.TestCase):
         out = au.sum_to_buffer(in1, outshape, in1_addr, out1_addr, dtype=FLOAT_TYPE)
         np.testing.assert_array_equal(out, expected_out)
 
+
+    def test_sum_to_buffer_complex(self):
+
+        I = 4
+        X = 2
+        M = 4
+        N = 4
+
+        in1 = np.empty((I, M, N), dtype=COMPLEX_TYPE)
+
+        # fill the input array
+        for idx in range(I):
+            in1[idx] = np.ones((M, N))* (idx + 1.0) + 1j * np.ones((M, N))* (idx + 1.0)
+
+        outshape = (X, M, N)
+        expected_out = np.empty(outshape, dtype=COMPLEX_TYPE)
+
+        expected_out[0] = np.ones((M, N)) * 4.0 + 1j * np.ones((M, N))* 4.0
+        expected_out[1] = np.ones((M, N)) * 6.0+ 1j * np.ones((M, N))* 6.0
+
+        in1_addr = np.empty((I, 3))
+
+        in1_addr = np.array([(0, 0, 0),
+                            (1, 0, 0),
+                            (2, 0, 0),
+                            (3, 0, 0)])
+
+        out1_addr = np.empty_like(in1_addr)
+        out1_addr = np.array([(0, 0, 0),
+                              (1, 0, 0),
+                              (0, 0, 0),
+                              (1, 0, 0)])
+
+        out = au.sum_to_buffer(in1, outshape, in1_addr, out1_addr, dtype=COMPLEX_TYPE)
+
+        np.testing.assert_array_equal(out, expected_out)
 
 if __name__=='__main__':
     unittest.main()
