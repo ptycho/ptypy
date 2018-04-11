68e900284d2e085629c2cf668185f4e930ccfd4a
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-04-11 09:52:03 +0100

    refactored the opi tests to be regression and unity based. This is much cleaner for someone trying to implement stuff.

--- ptypy/test/array_based_tests/object_probe_interaction_regression_test.py
+++ ptypy/test/array_based_tests/object_probe_interaction_regression_test.py
@@ -63,29 +63,6 @@ class ObjectProbeInteractionTest(unittest.TestCase):
                                                 exit_wave,
                                                 alpha=1.0)
 
-    def test_difference_map_realspace_constraint_UNITY(self):
-        PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
-        a_ptycho_instance = tu.get_ptycho_instance('pod_to_numpy_test')
-        # now convert to arrays
-        vectorised_scan = du.pod_to_arrays(PtychoInstance, 'S0000')
-        addr = vectorised_scan['meta'][
-            'addr']  # probably want to extract these at a later date, but just to get stuff going...
-        probe = vectorised_scan['probe']
-        obj = vectorised_scan['obj']
-        exit_wave = vectorised_scan['exit wave']
-
-        view_dlayer = 0  # what is this?
-        addr_info = addr[:, (view_dlayer)]  # addresses, object references
-        probe_and_object = opi.scan_and_multiply(probe, obj, exit_wave.shape, addr_info)
-
-        ptypy_dm_constraint = self.ptypy_apply_difference_map(a_ptycho_instance)
-        numpy_dm_constraint = opi.difference_map_realspace_constraint(probe_and_object,
-                                                                      exit_wave,
-                                                                      alpha=1.0)
-        for idx, key in enumerate(ptypy_dm_constraint):
-            np.testing.assert_allclose(ptypy_dm_constraint[key], numpy_dm_constraint[idx])
-
-
     def test_extract_array_from_exit_wave_regression_case_a(self):
         # two cases for this a) the array to be updated is bigger than the extracted array (which is the same size as the exit wave)
         #                    b) the other way round
@@ -611,16 +588,6 @@ class ObjectProbeInteractionTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(probe, not_shifted_probe, decimal=8) # interpolation obviously won't make this exact!
 
 
-
-    def ptypy_apply_difference_map(self, a_ptycho_instance):
-        f = OrderedDict()
-        alpha = 1.0
-        for dname, diff_view in a_ptycho_instance.diff.views.iteritems():
-            for name, pod in diff_view.pods.iteritems():
-                if not pod.active:
-                    continue
-                f[name] = (1 + alpha) * pod.probe * pod.object - alpha * pod.exit
-        return f
 
     if __name__ == "__main__":
         unittest.main()
---
+++ ptypy/test/array_based_tests/object_probe_interaction_unity_test.py
@@ -0,0 +1,49 @@
+'''
+This is a unity test comparing to the pod based framework
+'''
+
+import unittest
+import numpy as np
+import utils as tu
+from ptypy.array_based import COMPLEX_TYPE, FLOAT_TYPE
+from ptypy.array_based import data_utils as du
+from ptypy.array_based import object_probe_interaction as opi
+from collections import OrderedDict
+
+
+class ObjectProbeInteractionUnityTest(unittest.TestCase):
+
+    def test_difference_map_realspace_constraint_UNITY(self):
+        PtychoInstance = tu.get_ptycho_instance('pod_to_numpy_test')
+        a_ptycho_instance = tu.get_ptycho_instance('pod_to_numpy_test')
+        # now convert to arrays
+        vectorised_scan = du.pod_to_arrays(PtychoInstance, 'S0000')
+        addr = vectorised_scan['meta'][
+            'addr']  # probably want to extract these at a later date, but just to get stuff going...
+        probe = vectorised_scan['probe']
+        obj = vectorised_scan['obj']
+        exit_wave = vectorised_scan['exit wave']
+
+        view_dlayer = 0  # what is this?
+        addr_info = addr[:, (view_dlayer)]  # addresses, object references
+        probe_and_object = opi.scan_and_multiply(probe, obj, exit_wave.shape, addr_info)
+
+        ptypy_dm_constraint = self.ptypy_apply_difference_map(a_ptycho_instance)
+        numpy_dm_constraint = opi.difference_map_realspace_constraint(probe_and_object,
+                                                                      exit_wave,
+                                                                      alpha=1.0)
+        for idx, key in enumerate(ptypy_dm_constraint):
+            np.testing.assert_allclose(ptypy_dm_constraint[key], numpy_dm_constraint[idx])
+
+    def ptypy_apply_difference_map(self, a_ptycho_instance):
+        f = OrderedDict()
+        alpha = 1.0
+        for dname, diff_view in a_ptycho_instance.diff.views.iteritems():
+            for name, pod in diff_view.pods.iteritems():
+                if not pod.active:
+                    continue
+                f[name] = (1 + alpha) * pod.probe * pod.object - alpha * pod.exit
+        return f
+
+    if __name__ == "__main__":
+        unittest.main()
