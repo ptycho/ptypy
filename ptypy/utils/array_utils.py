ptycho/ptypy

commit ae2c04b2d80783dd2dbef4062f789f61bbc4510e

Made the test pass in core_dependency setting locally

ptypy/utils/array_utils.py
@@ -10,11 +10,12 @@ This file is part of the PTYPY package.
 
 import numpy as np
 from .misc import *
+from .math_utils import smooth_step
 
 __all__ = ['grids', 'switch_orientation', 'mirror',
            'crop_pad_symmetric_2d', 'crop_pad_axis', 'crop_pad',
            'pad_lr', 'zoom', 'shift_zoom', 'c_zoom',
-           'rebin', 'rebin_2d']
+           'rebin', 'rebin_2d', 'rectangle', 'ellipsis']
 
 
 def switch_orientation(A, orientation, center=None):
@@ -275,6 +276,23 @@ def grids(sh,psize=None,center='geometric',FFTlike=True):
             psize = psize * np.ones((len(sh),))
         psize = np.asarray(psize).reshape( (len(sh),) + len(sh)*(1,))
         return grid * psize
+
+def rectangle(grids, dims=None, ew=2):
+    if dims is None:
+        dims = (grids.shape[-2] / 2., grids.shape[-1] / 2.)
+    v, h = dims
+    V, H = grids
+    return (smooth_step(-np.abs(V) + v/2, ew)
+            * u.smooth_step(-np.abs(H) + h/2, ew))
+
+
+def ellipsis(grids, dims=None, ew=2):
+    if dims is None:
+        dims = (grids.shape[-2] / 2., grids.shape[-1] / 2.)
+    v, h = dims
+    V, H = grids
+    return smooth_step(
+        0.5 - np.sqrt(V**2/v**2 + H**2/h**2), ew/np.sqrt(v * h))
 
 def zoom(c,*arg,**kwargs):
     """

ptypy/core/data.py
@@ -1518,10 +1518,20 @@ class MoonFlowerScan(PtyScan):
         self.pixel = np.round(pixel).astype(int) + 10
         frame = self.pixel.max(0) + 10 + geo.shape
         self.geo = geo
-        self.obj = resources.flower_obj(frame)
-
+        
+        try:
+            self.obj = resources.flower_obj(frame)
+        except:
+            # matplotlib failsafe
+            self.obj = u.gf_2d(u.parallel.MPInoise2d(frame),1.)
+            
         # Get probe
-        moon = resources.moon_pr(self.geo.shape)
+        try:
+            moon = resources.moon_pr(self.geo.shape)
+        except:
+            # matplotlib failsafe
+            moon = u.ellipsis(u.grids(self.geo.shape)).astype(complex)
+        
         moon /= np.sqrt(u.abs2(moon).sum() / p.photons)
         self.pr = moon
         self.load_common_in_parallel = True

ptypy/core/illumination.py
@@ -202,24 +202,6 @@ DEFAULT_aperture = DEFAULT.aperture
 __all__ = ['init_storage', 'aperture', 'DEFAULT']
 
 
-def rectangle(grids, dims=None, ew=2):
-    if dims is None:
-        dims = (grids.shape[-2] / 2., grids.shape[-1] / 2.)
-    v, h = dims
-    V, H = grids
-    return (u.smooth_step(-np.abs(V) + v/2, ew)
-            * u.smooth_step(-np.abs(H) + h/2, ew))
-
-
-def ellipsis(grids, dims=None, ew=2):
-    if dims is None:
-        dims = (grids.shape[-2] / 2., grids.shape[-1] / 2.)
-    v, h = dims
-    V, H = grids
-    return u.smooth_step(
-        0.5 - np.sqrt(V**2/v**2 + H**2/h**2), ew/np.sqrt(v * h))
-
-
 def aperture(A, grids=None, pars=None, **kwargs):
     """
     Creates an aperture in the shape and dtype of `A` according
@@ -273,9 +255,9 @@ def aperture(A, grids=None, pars=None, **kwargs):
         grids[1] = cgrid.imag / psize[1]
 
         if str(p.form) == 'circ':
-            apert = lambda x: ellipsis(grids, x, p.edge)
+            apert = lambda x: u.ellipsis(grids, x, p.edge)
         elif str(p.form) == 'rect':
-            apert = lambda x: rectangle(grids, x, p.edge)
+            apert = lambda x: u.rectangle(grids, x, p.edge)
         else:
             raise NotImplementedError(
                 'Only elliptical `circ` or rectangular `rect` apertures are'

ptypy/core/manager.py
@@ -396,8 +396,9 @@ class ScanModel(object):
 
         parallel.allreduce(mean_frame)
         parallel.allreduce(norm)
-        parallel.allreduce(max_frame, parallel.MPI.MAX)
-        parallel.allreduce(max_frame, parallel.MPI.MIN)
+        if parallel.MPIenabled:
+            parallel.allreduce(max_frame, parallel.MPI.MAX)
+            parallel.allreduce(min_frame, parallel.MPI.MIN)
         mean_frame /= (norm + (norm == 0))
 
         self.diff.norm = norm

ptypy/resources/__init__.py
@@ -8,7 +8,7 @@ def flower_obj(shape=None):
     from ptypy import utils as u
     import numpy as np
     from matplotlib.image import imread
-
+    
     im = u.rgb2complex(imread(flowerfile))
     if shape is not None:
         sh = u.expect2(shape)
@@ -36,7 +36,7 @@ def moon_pr(shape=None):
     from ptypy import utils as u
     import numpy as np
     from matplotlib.image import imread
-
+    
     im = u.rgb2complex(imread(moonfile))
     if shape is not None:
         sh = u.expect2(shape)

ptypy/test/ptyscan_tests/csaxs_test.py
@@ -1,13 +1,13 @@
 
 import unittest
 from ptypy.test import utils as tu
-from ptypy.experiment.cSAXS import cSAXS
 from ptypy import utils as u
 
 
 class CsaxsTest(unittest.TestCase):
     @unittest.skip("This won't run without the data")
     def test_omny(self):
+        from ptypy.experiment.cSAXS import cSAXS
         r = u.Param()
         r.base_path = '/dls/i14/data/2017/cm16755-2/processing/cSAXS_May2017/'
         r.visit = 'e16403'

ptypy/test/template_tests/prep_and_run_DM_MF_multiple_probes_test.py
@@ -54,4 +54,6 @@ p.engines.engine00.fourier_relax_factor = 0.05
 
 # prepare and run
 P = Ptycho(p,level=4)
-P.plot_overview()
+
+# We shouldn't plot
+#P.plot_overview()

ptypy/utils/parallel.py
@@ -205,11 +205,12 @@ def _MPIop(a, op, axis=None):
     Apply operation op on accross a list of arrays distributed between
     processes. Supported operations are SUM, MAX, MIN, and PROD.
     """
+    
+    if MPIenabled:
+        MPIop = {'SUM': MPI.SUM, 'MAX': MPI.MAX, 'MIN': MPI.MIN, 'PROD': MPI.PROD}[op.upper()]
 
-    MPIop, npop = \
-        {'SUM': (MPI.SUM, np.sum), 'MAX': (MPI.MAX, np.max), 'MIN': (MPI.MIN, np.min), 'PROD': (MPI.PROD, np.prod)}[
-            op.upper()]
-
+    npop = {'SUM': np.sum, 'MAX':  np.max, 'MIN': np.min, 'PROD': np.prod}[op.upper()]
+    
     # Total op
     if axis is None:
         # Very special case: calling with an empty object might make sense in a few situations.

commit 48dd48dab53ffe1ab82cbe95ee5fa9765ac16286

Fixed bug introduced in ae2c04b2

ptypy/utils/array_utils.py
@@ -283,7 +283,7 @@ def rectangle(grids, dims=None, ew=2):
     v, h = dims
     V, H = grids
     return (smooth_step(-np.abs(V) + v/2, ew)
-            * u.smooth_step(-np.abs(H) + h/2, ew))
+            * smooth_step(-np.abs(H) + h/2, ew))
 
 
 def ellipsis(grids, dims=None, ew=2):