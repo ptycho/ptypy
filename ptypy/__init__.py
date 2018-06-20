a28221abce6443f8af19ad7bd349f9d1430c4f28
Pierre Thibault <pierre.thibault@soton.ac.uk>
2018-06-20 15:42:35 +0100

    Updated ptypy docstring

--- ptypy/__init__.py
+++ ptypy/__init__.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from .version import short_version, version, release
+
+__doc__ = \
 """
 PTYPY(v%(short)s): A ptychography reconstruction package.
 
 To cite PTYPY in publications, use
  @article{ ... }
 
-    :copyright: Copyright 2014 by the PTYPY team, see AUTHORS.
+    :copyright: Copyright 2018 by the PTYPY team, see AUTHORS.
     :license: GPLv2, see LICENSE for details.
     :version: %(version)s
     :status: %(devrel)s
 """ % {'version': version, 'devrel': 'release' if release else 'development', 'short': short_version}
 
 del short_version, release
-
-__zmq_msg = 'ZeroMQ not found.\nInteraction server & client disabled.\n\
-Install python-zmq via the package repositories or with `pip install --user pyzmq`'
-__mpi_msg = 'Message Passaging for Python (mpi4py) not found.\n\
-CPU-parallelization disabled.\n\
-Install python-mpi4py via the package repositories or with `pip install --user mpi4py`'
-__mpl_msg = 'Plotting for Python (matplotlib) not found.\n\
-Plotting disabled.\n\
-Install python-matplotlib via the package repositories or with `pip install --user matplotlib`'
 
 try:
     import zmq
@@ -54,8 +47,23 @@ from .utils import parallel
 # Logging
 from .utils import verbose
 
+# Log immediately dependency information
+if not __has_zmq__:
+    __zmq_msg = 'ZeroMQ not found.\nInteraction server & client disabled.\n\
+    Install python-zmq via the package repositories or with `pip install --user pyzmq`'
+    verbose.logger.warn(__zmq_msg)
+if not __has_mpi4py__:
+    __mpi_msg = 'Message Passaging for Python (mpi4py) not found.\n\
+    CPU-parallelization disabled.\n\
+    Install python-mpi4py via the package repositories or with `pip install --user mpi4py`'
+    verbose.logger.warn(__mpi_msg)
+if not __has_matplotlib__:
+    __mpl_msg = 'Plotting for Python (matplotlib) not found.\n\
+    Plotting disabled.\n\
+    Install python-matplotlib via the package repositories or with `pip install --user matplotlib`'
+    verbose.logger.warn(__mpl_msg)
+
 # Start a parameter tree
-#from .utils.descriptor import defaults_tree
 from .utils.descriptor import EvalDescriptor
 defaults_tree = EvalDescriptor('root')
 del EvalDescriptor
@@ -68,5 +76,4 @@ from . import experiment
 from . import core
 from . import simulations
 from . import resources
-
 
