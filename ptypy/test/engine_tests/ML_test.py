f3be1d60438b75c98c9a44ab90a53c3d41939129
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2018-11-26 10:22:37 +0000

    removes redundant test

--- ptypy/test/engine_tests/ML_test.py
+++ ptypy/test/engine_tests/ML_test.py
@@ -12,20 +12,6 @@ from ptypy import utils as u
 
 class MLTest(unittest.TestCase):
 
-    def test_ML_farfield(self):
-        engine_params = u.Param()
-        engine_params.name = 'ML'
-        engine_params.numiter = 5
-        engine_params.probe_update_start = 2
-        engine_params.floating_intensities = False
-        engine_params.intensity_renormalization = 1.0
-        engine_params.reg_del2 =True
-        engine_params.reg_del2_amplitude = 0.01
-        engine_params.smooth_gradient = 0.0
-        engine_params.scale_precond =False
-        engine_params.probe_update_start = 0
-        tu.EngineTestRunner(engine_params)
-
     def test_ML_farfield_floating_intensities(self):
         engine_params = u.Param()
         engine_params.name = 'ML'
