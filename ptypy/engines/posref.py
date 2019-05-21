5763b22a5da7991b1c888c3b68c1be80cdcbf2ff
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2019-05-21 10:20:49 +0100

    Updated the docstrings to numpy version

--- ptypy/engines/posref.py
+++ ptypy/engines/posref.py
@@ -36,20 +36,24 @@ class PositionRefine(object):
 
 
 class AnnealingRefine(PositionRefine):
-    """
-    Refines the positions by the following algorithm:
-    
-    A.M. Maiden, M.J. Humphry, M.C. Sarahan, B. Kraus, J.M. Rodenburg,
-    An annealing algorithm to correct positioning errors in ptychography,
-    Ultramicroscopy, Volume 120, 2012, Pages 64-72
-    """
-    def __init__(self, position_refinement_parameters, Cobj):
-        """
-        Annealing Positon Refinement.
 
-        :param position_refinement_parameters: See parameters defined in PositionCorrectionEngine
-        :param Cobj: Object Container
-        """
+    def __init__(self, position_refinement_parameters, Cobj):
+        '''
+        Annealing Position Refinement.
+        Refines the positions by the following algorithm:
+
+        A.M. Maiden, M.J. Humphry, M.C. Sarahan, B. Kraus, J.M. Rodenburg,
+        An annealing algorithm to correct positioning errors in ptychography,
+        Ultramicroscopy, Volume 120, 2012, Pages 64-72
+        Parameters
+        ----------
+        position_refinement_parameters : ptypy.utils.parameters.Param
+            The parameter tree for the refinement
+
+        Cobj : ptypy.core.classes.Container
+            The current pbject container object
+
+        '''
         super(AnnealingRefine, self).__init__(position_refinement_parameters)
 
         self.Cobj = Cobj  # take a reference here. It would be cool if we could make this read-only or something
@@ -74,34 +78,48 @@ class AnnealingRefine(PositionRefine):
         log(3, "Position refinement initialized")
 
     def fourier_error(self, di_view, obj):
-        """
-        Calculate the Fourier error based on a given diffraction and object.
+        '''
+        Parameters
+        ----------
+        di_view : ptypy.core.classes.View
+            A diffraction view for which we wish to calculate the error.
 
-        :param di_view: View to the diffraction pattern of the given position.
-        :param obj: Numpy array which contains the needed object.
-        :return: Fourier Error
-        """
+        obj : numpy.ndarray
+            The current calculated object for which we wish to evaluate the error against.
+        Returns
+        -------
+        np.float
+            The calculated fourier error
+        '''
+
         af2 = np.zeros_like(di_view.data)
         for name, pod in di_view.pods.iteritems():
             af2 += u.abs2(pod.fw(pod.probe*obj))
         return np.sum(di_view.pod.mask * (np.sqrt(af2) - np.sqrt(np.abs(di_view.data)))**2)
 
     def update_view_position(self, di_view):
-        """
+        '''
         Refines the positions by the following algorithm:
 
         A.M. Maiden, M.J. Humphry, M.C. Sarahan, B. Kraus, J.M. Rodenburg,
         An annealing algorithm to correct positioning errors in ptychography,
         Ultramicroscopy, Volume 120, 2012, Pages 64-72
-    
+
         Algorithm Description:
         Calculates random shifts around the original position and calculates the fourier error. If the fourier error
         decreased the randomly calculated postion will be used as new position.
-    
-        :param di_view: Diffraction view for which a better position is searched for.
 
-        :return The best shift found (for eventual statistics)
-        """
+        Parameters
+        ----------
+        di_view : ptypy.core.classes.View
+            A diffraction view that we wish to refine.
+
+        Returns
+        -------
+        numpy.ndarray
+            A length 2 numpy array with the position increments for x and y co-ordinates respectively
+        '''
+
         dcoords = np.zeros((self.p.nshifts + 1, 2)) - 1.    # Coordinates (including the original)
         delta = np.zeros((self.p.nshifts, 2))               # Coordinate shifts
         errors = np.zeros(self.p.nshifts)                   # Calculated errors for the shifted positions
@@ -158,10 +176,8 @@ class AnnealingRefine(PositionRefine):
 
         Parameters
         ----------
-        [iteration]
-        help = The current iteration of the engine.
-        type = int
-
+        iteration : int
+            The current iteration of the engine.
         '''
 
         start, end = self.p.start, self.p.stop
@@ -182,7 +198,8 @@ class AnnealingRefine(PositionRefine):
         '''
         Returns
         -------
-        List of container names to cleanup.
+        List
+            List of container names to cleanup.
         '''
         container_names = []
         self.temp_ob.original = self.temp_ob  # unlink from original
