e5586a67a4d6afbc31b42c4e3f8acadd3c6f89b4
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2019-07-17 12:16:19 +0100

    I mislaid the parameter. Now I add it back into the docstring

--- ptypy/experiment/hdf5_loader.py
+++ ptypy/experiment/hdf5_loader.py
@@ -224,6 +224,11 @@ class Hdf5Loader(PtyScan):
     type = str
     help = This is the key to the recorded_energy entry in the hdf5 file.
 
+    [recorded_energy.multiplier]
+    default = 1.0
+    type = float
+    help = This is the multiplier for the recorded energy.
+
     [recorded_distance]
     default =
     type = Param
@@ -380,6 +385,7 @@ class Hdf5Loader(PtyScan):
             log(3, "No normalisation will be applied.")
 
         if None not in [self.p.recorded_energy.file, self.p.recorded_energy.key]:
+            print self.p.recorded_energy.multiplier
             self.p.energy = np.float(h5.File(self.p.recorded_energy.file, 'r')[self.p.recorded_energy.key][()] * self.p.recorded_energy.multiplier)
             self.meta.energy  = self.p.energy
             log(3, "loading energy={} from file".format(self.p.energy))
