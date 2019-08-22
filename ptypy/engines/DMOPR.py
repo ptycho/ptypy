b264b4202cc4945e284a04327003bf2bc034a3fb
Benedikt Daurer <bjdaurer@gmail.com>
2019-08-22 13:55:34 +0100

    Implement probe change similar to regular DM

--- ptypy/engines/DMOPR.py
+++ ptypy/engines/DMOPR.py
@@ -119,7 +119,7 @@ class DMOPR(DM):
         
             # Compute relative change in probe
             buf = pr_buf.storages[name].data
-            change += u.norm2(s.data - buf) #/ u.norm2(s.data)
+            change += u.norm2(s.data - buf) / u.norm2(s.data)
 
             # Fill buffer with new probe
             buf[:] = s.data
