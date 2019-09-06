4abe6ff23362335f0e38af09519e17e61811cf23
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2019-09-06 16:38:13 +0100

    This isn't an integer in p3. It needs to be

--- ptypy/engines/base.py
+++ ptypy/engines/base.py
@@ -333,7 +333,7 @@ class PositionCorrectionEngine(BaseEngine):
             # Enlarge object arrays, 
             # This can be skipped though if the boundary is less important
             for name, s in self.ob.storages.items():
-                s.padding = self.p.position_refinement.max_shift // np.max(s.psize)
+                s.padding = int(self.p.position_refinement.max_shift // np.max(s.psize))
                 s.reformat()
 
             # this could be some kind of dictionary lookup if we want to add more
