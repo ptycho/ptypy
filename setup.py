4a9bf1c53aa366944928204ea8814f1f5c5e195c
Bjoern Enders <benders@lbl.gov>
2019-12-10 10:45:58 +0000

    Set to non-release for versioning

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ Operating System :: Unix
 MAJOR               = 0
 MINOR               = 4
 MICRO               = 0
-ISRELEASED          = True
+ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 #import os
