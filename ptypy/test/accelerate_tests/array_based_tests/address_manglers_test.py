546ef9b9bbef61148b0a175e7f5687714492cf65
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2020-03-06 14:37:27 +0000

    fixes address mangler test. it was just out of date

--- ptypy/test/accelerate_tests/array_based_tests/address_manglers_test.py
+++ ptypy/test/accelerate_tests/array_based_tests/address_manglers_test.py
@@ -16,7 +16,6 @@ class AddressManglersTest(unittest.TestCase):
     def tearDown(self):
         np.set_printoptions()
 
-
     def test_addr_original_set(self):
 
         max_bound = 10
@@ -29,7 +28,7 @@ class AddressManglersTest(unittest.TestCase):
         X = X.reshape((total_number_scan_positions)) + max_bound  # max bound is added in the DM_serial engine.
         Y = Y.reshape((total_number_scan_positions)) + max_bound
 
-        addr = np.zeros((total_number_scan_positions, num_modes, 5, 3), dtype=INT_TYPE)
+        addr_original = np.zeros((total_number_scan_positions, num_modes, 5, 3), dtype=INT_TYPE)
 
         exit_idx = 0
         position_idx = 0
@@ -37,7 +36,7 @@ class AddressManglersTest(unittest.TestCase):
             mode_idx = 0
             for pr_mode in range(num_modes):
                 for ob_mode in range(1):
-                    addr[position_idx, mode_idx] = np.array([[pr_mode, 0, 0],
+                    addr_original[position_idx, mode_idx] = np.array([[pr_mode, 0, 0],
                                                              [ob_mode, ypos, xpos],
                                                              [exit_idx, 0, 0],
                                                              [0, 0, 0],
@@ -46,27 +45,24 @@ class AddressManglersTest(unittest.TestCase):
                     exit_idx += 1
             position_idx += 1
 
-        print(repr(addr))
+        print(repr(addr_original))
 
         old_positions = np.zeros((total_number_scan_positions))
 
-        differences_from_original = np.zeros((len(addr), 2))
+        differences_from_original = np.zeros((len(addr_original), 2))
         differences_from_original[::2] = 12  # so definitely more than the max_bound
-        new_positions = addr[:, 0, 1, 1:] + differences_from_original
+        new_positions = addr_original[:, 0, 1, 1:] + differences_from_original
 
-        mangler = RandomIntMangle(max_step_per_shift=step_size, max_bound=max_bound)
+        mangler = RandomIntMangle(step_size, 50, 100, max_bound=max_bound, )
 
-        # manually set the original_addr
-        mangler.addr_original = addr
 
-        mangler.apply_bounding_box(new_positions, old_positions)
+        mangler.apply_bounding_box(new_positions, old_positions, addr_original)
         print(repr(new_positions))
         expected_new_positions = new_positions[:]
         expected_new_positions[::2] = 0
 
         print(repr(expected_new_positions))
 
-        np.testing.assert_array_equal(expected_new_positions, new_positions)
         np.testing.assert_array_equal(expected_new_positions, new_positions)
 
 
