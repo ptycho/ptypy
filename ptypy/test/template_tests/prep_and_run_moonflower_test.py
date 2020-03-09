d98185ca00d4e95736e33ae277fcf628cd4c0e31
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2020-03-09 10:47:01 +0000

    Make sure template tests run fast

--- ptypy/test/template_tests/prep_and_run_moonflower_test.py
+++ ptypy/test/template_tests/prep_and_run_moonflower_test.py
@@ -10,15 +10,19 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p = u.Param()
         p.verbose_level = 3
         p.io = u.Param()
-        p.io.home = tempfile.mkdtemp()
+        p.io.interaction = u.Param()
+        p.io.interaction.active = False
+        p.io.home = "./"
+        p.io.rfile = "None.ptyr"
         p.io.autosave = u.Param(active=False)
         p.io.autoplot = u.Param(active=False)
+        p.ipython_kernel = False
         p.scans = u.Param()
         p.scans.MF = u.Param()
         p.scans.MF.name = 'Full'
         p.scans.MF.data= u.Param()
         p.scans.MF.data.name = 'MoonFlowerScan'
-        p.scans.MF.data.shape = 128
+        p.scans.MF.data.shape = 64
         p.scans.MF.data.num_frames = 100
         p.scans.MF.data.save = None
         p.scans.MF.data.density = 0.2
@@ -29,21 +33,25 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p.engines.engine00.name = 'DM'
         p.engines.engine00.numiter = 5
         P = Ptycho(p,level=5)
-        return
+        return P
 
     def test_dm_multiple_probes(self):
         p = u.Param()
         p.verbose_level = 3
         p.io = u.Param()
-        p.io.home = tempfile.mkdtemp()
+        p.io.interaction = u.Param()
+        p.io.interaction.active = False
+        p.io.home = "./"
+        p.io.rfile = "None.ptyr"
         p.io.autosave = u.Param(active=False)
         p.io.autoplot = u.Param(active=False)
+        p.ipython_kernel = False
         p.scans = u.Param()
         p.scans.MF = u.Param()
         p.scans.MF.name = 'Full'
         p.scans.MF.data= u.Param()
         p.scans.MF.data.name = 'MoonFlowerScan'
-        p.scans.MF.data.shape = 128
+        p.scans.MF.data.shape = 64
         p.scans.MF.data.num_frames = 200
         p.scans.MF.data.save = None
         p.scans.MF.data.density = 0.15
@@ -56,24 +64,28 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p.engines = u.Param()
         p.engines.engine00 = u.Param()
         p.engines.engine00.name = 'DM'
-        p.engines.engine00.numiter = 130
+        p.engines.engine00.numiter = 5
         p.engines.engine00.fourier_relax_factor = 0.05
         P = Ptycho(p,level=5)
-        return
+        return P
 
     def test_dm_resample(self):
         p = u.Param()
         p.verbose_level = 3
         p.io = u.Param()
-        p.io.home = tempfile.mkdtemp()
+        p.io.interaction = u.Param()
+        p.io.interaction.active = False
+        p.io.home = "./"
+        p.io.rfile = "None.ptyr"
         p.io.autosave = u.Param(active=False)
         p.io.autoplot = u.Param(active=False)
+        p.ipython_kernel = False
         p.scans = u.Param()
         p.scans.MF = u.Param()
         p.scans.MF.name = 'Full'
         p.scans.MF.data= u.Param()
         p.scans.MF.data.name = 'MoonFlowerScan'
-        p.scans.MF.data.shape = 128
+        p.scans.MF.data.shape = 64
         p.scans.MF.data.num_frames = 100
         p.scans.MF.data.save = None
         p.scans.MF.data.density = 0.2
@@ -85,21 +97,25 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p.engines.engine00.name = 'DM'
         p.engines.engine00.numiter = 5
         P = Ptycho(p,level=5)
-        return
+        return P
 
     def test_ml_single_probe(self):
         p = u.Param()
         p.verbose_level = 3
         p.io = u.Param()
-        p.io.home = tempfile.mkdtemp()
+        p.io.interaction = u.Param()
+        p.io.interaction.active = False
+        p.io.home = "./"
+        p.io.rfile = "None.ptyr"
         p.io.autosave = u.Param(active=False)
         p.io.autoplot = u.Param(active=False)
+        p.ipython_kernel = False
         p.scans = u.Param()
         p.scans.MF = u.Param()
         p.scans.MF.name = 'Full'
         p.scans.MF.data= u.Param()
         p.scans.MF.data.name = 'MoonFlowerScan'
-        p.scans.MF.data.shape = 128
+        p.scans.MF.data.shape = 64
         p.scans.MF.data.num_frames = 100
         p.scans.MF.data.save = None
         p.scans.MF.data.density = 0.2
@@ -116,29 +132,33 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p.engines.engine00.smooth_gradient = 20.
         p.engines.engine00.smooth_gradient_decay = 1/50.
         p.engines.engine00.floating_intensities = False
-        p.engines.engine00.numiter = 300
+        p.engines.engine00.numiter = 5
         P = Ptycho(p,level=5)
-        return
+        return P
 
     def test_ml_resample(self):
         p = u.Param()
         p.verbose_level = 3
         p.io = u.Param()
-        p.io.home = tempfile.mkdtemp()
+        p.io.interaction = u.Param()
+        p.io.interaction.active = False
+        p.io.home = "./"
+        p.io.rfile = "None.ptyr"
         p.io.autosave = u.Param(active=False)
         p.io.autoplot = u.Param(active=False)
+        p.ipython_kernel = False
         p.scans = u.Param()
         p.scans.MF = u.Param()
         p.scans.MF.name = 'Full'
         p.scans.MF.data= u.Param()
         p.scans.MF.data.name = 'MoonFlowerScan'
-        p.scans.MF.data.shape = 128
+        p.scans.MF.data.shape = 64
         p.scans.MF.data.num_frames = 100
         p.scans.MF.data.save = None
         p.scans.MF.data.density = 0.2
         p.scans.MF.data.photons = 1e8
         p.scans.MF.data.psf = 0.
-        p.scans.ML.resample = 2
+        p.scans.MF.resample = 2
         p.engines = u.Param()
         p.engines.engine00 = u.Param()
         p.engines.engine00.name = 'ML'
@@ -150,9 +170,9 @@ class PrepAndRunMoonFlowerTest(unittest.TestCase):
         p.engines.engine00.smooth_gradient = 20.
         p.engines.engine00.smooth_gradient_decay = 1/50.
         p.engines.engine00.floating_intensities = False
-        p.engines.engine00.numiter = 300
+        p.engines.engine00.numiter = 5
         P = Ptycho(p,level=5)
-        return
+        return P
 
 if __name__ == '__main__':
     unittest.main()
