9071a10bbafa9bfe237f2a69931f65a1a329b6d6
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2020-04-01 17:49:48 +0100

    initial refactor. Now subclasses the UnixCCompiler like a real person

--- ptypy/accelerate/py_cuda/import_fft.py
+++ ptypy/accelerate/py_cuda/import_fft.py
@@ -4,7 +4,7 @@
 # import shutil
 import os
 from distutils.sysconfig import get_config_var
-
+from distutils.unixccompiler import UnixCCompiler
 import importlib
 import setuptools
 import setuptools.command.build_ext
@@ -67,115 +67,82 @@ def locate_cuda():
 
     return cudaconfig
 
-
-def customize_compiler_for_nvcc(self):
-    """inject deep into distutils to customize how the dispatch
-    to gcc/nvcc works.
-
-    If you subclass UnixCCompiler, it's not trivial to get your subclass
-    injected in, and still have the right customizations (i.e.
-    distutils.sysconfig.customize_compiler) run on it. So instead of going
-    the OO route, I have this. Note, it's kindof like a wierd functional
-    subclassing going on."""
-
-    # tell the compiler it can processes .cu
-    self.src_extensions.append('.cu')
-
-    # save references to the default compiler_so and _comple methods
-    default_compiler_so = self.compiler_so
-    default_linker_so = self.linker_so
-
-    original__compile = self._compile
-    original_link = self.link
-    CUDA = locate_cuda()
-    # now redefine the _compile method. This gets executed for each
-    # object but distutils doesn't have the ability to change compilers
-    # based on source extension: we add it.
-    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-        # if os.path.splitext(src)[1] == '.cpp':
-            # use the cuda for .cu files
+class NvccCompiler(UnixCCompiler):
+    def __init__(self, *args, **kwargs):
+        super(NvccCompiler, self).__init__(*args, **kwargs)
+        # we only need this if it's not always in a fixed path. I think we can probably assume this.
+        # we could probably just give the module dir directly since we know where it is
         full_module_name = "ptypy.accelerate.py_cuda.cuda.filtered_fft.module"
         module_file_path = find.find_module_cpppath(full_module_name)
-    
-        nvcc_path = find_in_path('nvcc', os.environ['PATH'])
-        if nvcc_path is None or not os.path.isfile(nvcc_path):
-            raise EnvironmentError('The nvcc binary could not be located in your'
-                                    ' $PATH. Either add it to your path, or set'
-                                    ' appropiate $CUDAHOME.')
-
-        CUDADIR = os.path.dirname(nvcc_path)        
         module_dir = os.path.dirname(module_file_path)
+        self.CUDA = locate_cuda()
         cmp = driver.Context.get_device().compute_capability()
-        # print(dev.compute_capability())
         archflag = '-arch=sm_{}{}'.format(cmp[0], cmp[1])
-        pybind_includes = [pybind11.get_include(True)]
-        INCLUDES = pybind_includes + [CUDA['lib64'], module_dir]
-        INCLUDES = ["-I%s" % ix for ix in INCLUDES]
-        # PYMODEXT = $(shell python-config --extension-suffix)
-        PYMODEXT = '.so'
-        CPPFLAGS = INCLUDES + extra_postargs
-        OPTFLAGS = ["-O3", "-std=c++14"]
-        CXXFLAGS = ['"-fPIC"']
-        NVCC_FLAGS = ["-dc", archflag]
-        LD_FLAGS = ["-lcufft_static", "-lculibos", "-ldl", "-lrt", "-lpthread", "-cudart shared"]
-        # $(NVCC) $(NVCC_FLAGS) $(OPTFLAGS) -Xcompiler "$(CXXFLAGS)" $(CPPFLAGS)
-        compiler_command = [CUDA["nvcc"]] + NVCC_FLAGS + OPTFLAGS + ["-Xcompiler"] + CXXFLAGS + CPPFLAGS
-        compiler_exec = " ".join(compiler_command)
+        self.src_extensions.append('.cu')
+        self.LD_FLAGS = ["-lcufft_static", "-lculibos", "-ldl", "-lrt", "-lpthread", "-cudart shared"]
+        self.NVCC_FLAGS = ["-dc", archflag]
+        self.CXXFLAGS = ['"-fPIC"']
+        pybind_includes = [pybind11.get_include(), pybind11.get_include(True)+'/python3.7m/']  # why has pybind includes stopped giving me the python3,7m directory?
+        INCLUDES = pybind_includes + [self.CUDA['lib64'], module_dir]
+        self.INCLUDES = ["-I%s" % ix for ix in INCLUDES]
+        self.OPTFLAGS = ["-O3", "-std=c++14"]
 
+    def _compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
+        # if os.path.splitext(src)[1] == '.cpp':
+            # use the cuda for .cu files
+        default_compiler_so = self.compiler_so
+
+
+        CPPFLAGS = self.INCLUDES + extra_postargs
+        # makefile line is
+        # $(NVCC) $(NVCC_FLAGS) $(OPTFLAGS) -Xcompiler "$(CXXFLAGS)" $(CPPFLAGS)
+        compiler_command = [self.CUDA["nvcc"]] + self.NVCC_FLAGS + self.OPTFLAGS + ["-Xcompiler"] + self.CXXFLAGS + CPPFLAGS
+        compiler_exec = " ".join(compiler_command)
         self.set_executable('compiler_so', compiler_exec)
 
-
-        # use only a subset of the extra_postargs, which are 1-1 translated
-        # from the extra_compile_args in the Extension class
         postargs = []
         pp = pp_opts.append([])
-        # else:
-        #     postargs = extra_postargs['gcc']
 
-        original__compile(obj, src, ext, cc_args, postargs, pp) # the _compile method
+        super(NvccCompiler, self)._compile(obj, src, ext, cc_args, postargs, pp) # the _compile method
         # reset the default compiler_so, which we might have changed for cuda
         self.compiler_so = default_compiler_so
-
-    # inject our redefined _compile method into the class
-    self._compile = _compile
-
-    def link(target_desc, objects,
+    
+    def link(self, target_desc, objects,
              output_filename, output_dir=None, libraries=None,
              library_dirs=None, runtime_library_dirs=None,
              export_symbols=None, debug=0, extra_preargs=None,
              extra_postargs=None, build_temp=None, target_lang=None):
-        OPTFLAGS = ["-O3", "-std=c++14"]
-        LD_FLAGS = ["-lcufft_static", "-lculibos", "-ldl", "-lrt", "-lpthread", "-cudart shared"]
+        default_linker_so = self.linker_so
 
-        linker_command = [CUDA["nvcc"]] + OPTFLAGS + ["-shared"] + LD_FLAGS
+        linker_command = [self.CUDA["nvcc"]] + self.OPTFLAGS + ["-shared"] + self.LD_FLAGS
         linker_exec = " ".join(linker_command)
 
         self.set_executable('linker_so', linker_exec)
-        original_link(target_desc, objects,
+        super(NvccCompiler, self).link(target_desc, objects,
              output_filename, output_dir=None, libraries=None,
              library_dirs=None, runtime_library_dirs=None,
              export_symbols=None, debug=0, extra_preargs=None,
              extra_postargs=None, build_temp=None, target_lang=None)
         self.linker_so = default_linker_so
-    self.link = link
+
 
 # run the customize_compiler
 class custom_build_ext(build_ext):
     def build_extensions(self):
-        customize_compiler_for_nvcc(self.compiler)
+        old_compiler = self.compiler
+        self.compiler = NvccCompiler(verbose=old_compiler.verbose,
+                                    dry_run=old_compiler.dry_run,
+                                    force=old_compiler.force) # this is our bespoke compiler
         build_ext.build_extensions(self)
+        self.compiler=old_compiler
 
 
 def import_fft(rows, columns, build_path=None):
     if build_path is None:
         build_path = tempfile.mkdtemp(prefix="extension_tests")
-
-
- # we only need this if it's not always in a fixed path. I think we can probably assume this.
-
-    CUDA = locate_cuda()
+    # we only need this if it's not always in a fixed path. I think we can probably assume this.
+    # we could probably just give the module dir directly since we know where it is
     full_module_name = "ptypy.accelerate.py_cuda.cuda.filtered_fft.module"
-
     module_file_path = find.find_module_cpppath(full_module_name)
     module_dir = os.path.dirname(module_file_path)
 
@@ -184,13 +151,8 @@ def import_fft(rows, columns, build_path=None):
                     library_dirs=[],
                     libraries=[], # distuils adds a -l infront of all of these (add_library_option:https://github.com/python/cpython/blob/1c1e68cf3e3a2a19a0edca9a105273e11ddddc6e/Lib/distutils/ccompiler.py#L1115)
                     runtime_library_dirs=[],
-                    # this syntax is specific to this build system
-                    # we're only going to use certain compiler args with nvcc and not with gcc
-                    # the implementation of this trick is in customize_compiler() below
                     extra_compile_args=["-DMY_FFT_COLS=%s" % str(columns) , "-DMY_FFT_ROWS=%s" % str(rows)],
                     include_dirs=[])
-
-
 
     script_args = ['build_ext',
                    '--build-temp=%s' % build_path,
@@ -200,8 +162,8 @@ def import_fft(rows, columns, build_path=None):
                        "script_args": script_args,
                        "cmdclass":{"build_ext": custom_build_ext}
                        }
-    setuptools.setup(**setuptools_args)
 
+    setuptools.setup(**setuptools_args)
 
     spec = importlib.util.spec_from_file_location('module',
                                                   os.path.join(build_path,
--- ptypy/test/accelerate_tests/py_cuda_tests/fft_tests/fft_import_fft_test.py
+++ ptypy/test/accelerate_tests/py_cuda_tests/fft_tests/fft_import_fft_test.py
@@ -16,62 +16,12 @@ class ImportFFTTest(PyCudaTest):
         import_fft.import_fft(32, 32)
     
     
-    def test_import_fft_twice(self):
+    def test_import_fft_different_shape(self):
         import_fft.import_fft(128, 128)
     
-    def test_import_fft_twice_again(self):
+    def test_import_fft_same_module_again(self):
         import_fft.import_fft(32, 32)
 
-    # def test_32_32(self):
-    #     rows = columns = 32
-    #     module_name = 'module_' + str(rows) + '_' + str(columns)
-    #     dirname = os.path.join("/home/clb02321/PycharmProjects/ptypy_accelerate_fresh/build/lib/ptypy/accelerate/py_cuda", 'cuda', 'filtered_fft')
-    #     dst  = os.path.join(dirname, module_name)
-    #     src = dirname # os.path.join(dirname, 'module.cpp')
-    #     # dst = os.path.join(dirname, module_name + '.cpp')
-    #     shutil.copytree(src, dst)
-    #     # print('copies {} to {}'.format(src, dst))
-    #
-    #     # monkey-patch the customize_compiler function
-    #     old = sysconfig.customize_compiler
-    #     sysconfig.customize_compiler = get_customize_compiler(rows, columns, old)
-    #
-    #     import cppimport
-#     # cppimport.force_rebuild()
-    #     # cppimport.set_quiet(True)
-    #     # cppimport
-    #     import_module_name = "ptypy.accelerate.py_cuda.cuda.filtered_fft.%s.module" % module_name
-    #     print("Import module name is %s" % import_module_name)
-    #     filtered_fft = cppimport.imp(import_module_name)
-    #
-    #     # revert the monkey-patch
-    #     sysconfig.customize_compiler = old
-    #
-    #
-    # def test_128_128(self):
-    #     rows = columns = 128
-    #     module_name = 'module_' + str(rows) + '_' + str(columns)
-    #     dirname = os.path.join("/home/clb02321/PycharmProjects/ptypy_accelerate_fresh/build/lib/ptypy/accelerate/py_cuda", 'cuda', 'filtered_fft')
-    #     dst  = os.path.join(dirname, module_name)
-    #     src = dirname # os.path.join(dirname, 'module.cpp')
-    #     # dst = os.path.join(dirname, module_name + '.cpp')
-    #     shutil.copytree(src, dst)
-    #     # print('copies {} to {}'.format(src, dst))
-    #
-    #     # monkey-patch the customize_compiler function
-    #     old = sysconfig.customize_compiler
-    #     sysconfig.customize_compiler = get_customize_compiler(rows, columns, old)
-    #
-    #     import cppimport
-    #     # cppimport.force_rebuild()
-    #     # cppimport.set_quiet(True)
-    #     # cppimport
-    #     import_module_name = "ptypy.accelerate.py_cuda.cuda.filtered_fft.%s.module" % module_name
-    #     print("Import module name is %s" % import_module_name)
-    #     filtered_fft = cppimport.imp(import_module_name)
-    #
-    #     # revert the monkey-patch
-    #     sysconfig.customize_compiler = old
 
 if __name__=="__main__":
     unittest.main()
