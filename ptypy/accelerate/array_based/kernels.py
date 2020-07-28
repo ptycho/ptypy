Added memory to ML_pycuda but it's not used yet. Smaller fixes like LL and photon error now follow original more closely. Found wrong calculation of A1, A2 in polyline coefficient

ptypy/accelerate/array_based/kernels.py
@@ -210,11 +210,11 @@ class GradientDescentKernel(BaseKernel):
 
         A1.fill(0.)
         tf = 2. * np.real(f * a.conj())
-        A1[:maxz] = tf.reshape(maxz, self.nmodes, sh[1], sh[2]).sum(1) - I
+        A1[:maxz] = tf.reshape(maxz, self.nmodes, sh[1], sh[2]).sum(1)
 
         A2.fill(0.)
         tf = 2. * np.real(f * b.conj()) + np.abs(a) ** 2
-        A2[:maxz] = tf.reshape(maxz, self.nmodes, sh[1], sh[2]).sum(1) - I
+        A2[:maxz] = tf.reshape(maxz, self.nmodes, sh[1], sh[2]).sum(1)
         return
 
     def fill_b(self, addr, Brenorm, w, B):

ptypy/engines/ML.py
@@ -291,7 +291,7 @@ class ML(PositionCorrectionEngine):
             t2 = time.time()
             B = self.ML_model.poly_line_coeffs(self.ob_h, self.pr_h)
             tc += time.time() - t2
-            print(B, Cnorm2(self.ob_h), Cnorm2(self.ob_grad), Cnorm2(self.pr_h), Cnorm2(self.pr_grad))
+            #print(B, Cnorm2(self.ob_h), Cnorm2(self.ob_grad), Cnorm2(self.pr_h), Cnorm2(self.pr_grad))
             if np.isinf(B).any() or np.isnan(B).any():
                 logger.warning(
                     'Warning! inf or nan found! Trying to continue...')
@@ -511,7 +511,7 @@ class GaussianModel(BaseModel):
                 LL += self.regularizer.LL
 
         self.LL = LL / self.tot_measpts
-
+        print(self.LL)
         return error_dct
 
     def poly_line_coeffs(self, ob_h, pr_h):

ptypy/engines/ML_pycuda.py
@@ -16,6 +16,7 @@ import time
 from pycuda import gpuarray
 import pycuda.driver as cuda
 from pycuda.tools import DeviceMemoryPool
+from collections import deque
 
 from . import register
 from .ML import ML, BaseModel, prepare_smoothing_preconditioner, Regul_del2
@@ -31,6 +32,88 @@ from ..accelerate.array_based import address_manglers
 
 __all__ = ['ML_pycuda']
 
+
+class MemoryManager:
+
+    def __init__(self, fraction=0.7):
+        self.fraction = fraction
+        self.dmp = DeviceMemoryPool()
+        self.queue_in = cuda.Stream()
+        self.queue_out = cuda.Stream()
+        self.mem_avail = None
+        self.mem_total = None
+        self.get_free_memory()
+        self.on_device = {}
+        self.on_device_inv = {}
+        self.out_events = deque()
+        self.bytes = 0
+
+    def get_free_memory(self):
+        self.mem_avail, self.mem_total = cuda.mem_get_info()
+
+    def device_is_full(self, nbytes = 0):
+        return (nbytes + self.bytes) > self.mem_avail
+
+    def to_gpu(self, ar, ev=None):
+        """
+        Issues asynchronous copy to device. Waits for optional event ev
+        Emits event for other streams to synchronize with
+        """
+        stream = self.queue_in
+        id_cpu = id(ar)
+        gpu_ar = self.on_device.get(id_cpu)
+
+        if gpu_ar is None:
+            if ev is not None:
+                stream.wait_for_event(ev)
+            if self.device_is_full(ar.nbytes):
+                self.wait_for_freeing_events(ar.nbytes)
+
+            # TOD0: try /except with garbage collection to make sure there is space
+            gpu_ar = gpuarray.to_gpu_async(ar, allocator=self.dmp.allocate, stream=stream)
+
+            # keeps gpuarray alive
+            self.on_device[id_cpu] = gpu_ar
+
+            # for deleting later
+            self.on_device_inv[id(gpu_ar)] = ar
+
+            self.bytes += gpu_ar.mem_size * gpu_ar.dtype.itemsize
+
+
+        ev = cuda.Event()
+        ev.record(stream)
+        return ev, gpu_ar
+
+
+    def wait_for_freeing_events(self, nbytes):
+        """
+        Wait until at least nbytes have been copied back to the host. Or marked for deletion
+        """
+        freed = 0
+        if not self.out_events:
+            #print('Waiting for memory to be released on device failed as no release event was scheduled')
+            self.queue_out.synchronize()
+        while self.out_events and freed < nbytes:
+            ev, id_cpu, id_gpu = self.out_events.popleft()
+            gpu_ar = self.on_device.pop(id_cpu)
+            cpu_ar = self.on_device_inv.pop(id_gpu)
+            ev.synchronize()
+            freed += cpu_ar.nbytes
+            self.bytes -= gpu_ar.mem_size * gpu_ar.dtype.itemsize
+
+    def mark_release_from_gpu(self, gpu_ar, to_cpu=False, ev=None):
+        stream = self.queue_out
+        if ev is not None:
+            stream.wait_for_event(ev)
+        if to_cpu:
+            cpu_ar = self.on_device_inv[id(gpu_ar)]
+            gpu_ar.get_asynch(stream, host_array)
+
+        ev_out = cuda.Event()
+        ev_out.record(stream)
+        self.out_events.append((ev_out, id(cpu_ar), id(gpu_ar)))
+        return ev_out
 
 
 @register()
@@ -113,9 +196,6 @@ class ML_pycuda(ML_serial):
             kern.GDK = GradientDescentKernel(aux, nmodes, queue=self.queue)
             kern.GDK.allocate()
 
-            #kern.GDKs = GDK_serial(aux.get(), nmodes)
-            #kern.GDKs.allocate()
-
             kern.POK = PoUpdateKernel(queue_thread=self.queue, denom_type=np.float32)
             kern.POK.allocate()
 
@@ -173,10 +253,12 @@ class ML_pycuda(ML_serial):
                         if sync_copy: s.gpu.set(s.cpu)
                     elif dev == 'cpu':
                         s.data = s.cpu
-                        if sync_copy: s.gpu.get(s.cpu)
+                        if sync_copy:
+                            s.gpu.get(s.cpu)
+                            #print('%s to cpu' % s.ID)
         else:
             for container in self.ptycho.containers.values():
-                self._set_pr_ob_ref_for_data(dev=dev, container=container)
+                self._set_pr_ob_ref_for_data(dev=dev, container=container, sync_copy=sync_copy)
 
     def _replace_ob_grad(self):
         new_ob_grad = self.ob_grad_new
@@ -339,6 +421,7 @@ class GaussianModel(BaseModelSerial):
             #w = gpuarray.to_gpu(prep.weights)
             #I = gpuarray.to_gpu(prep.I)
             stream = self.engine.queue_transfer
+            # TODO keep alive
             w = gpuarray.to_gpu_async(prep.weights, allocator=self.engine.dmp.allocate, stream=stream)
             I = gpuarray.to_gpu_async(prep.I, allocator=self.engine.dmp.allocate, stream=stream)
             ev = cuda.Event()
@@ -360,29 +443,15 @@ class GaussianModel(BaseModelSerial):
                 GDK.error_reduce(err_den, w * Imodel ** 2)
                 Imodel *= (err_num / err_den).reshape(Imodel.shape[0], 1, 1)
             """
-            #LLerr = GDK.gpu.LLerr.get()
-            #print(np.allclose(GDK.gpu.Imodel.get(), kern.GDKs.npy.Imodel))
-            #print(np.isnan(LLerr).any())
-            #print(np.isnan(GDK.gpu.Imodel.get()).any())
-            #print(np.isnan(I.get()).any())
-            #print(np.isnan(aux.get()).any())
-            #aux2 = aux.get()
+
             GDK.queue.wait_for_event(ev)
             GDK.main(aux, addr, w, I)
-            #kern.GDKs.main(aux2, addr.get(), w.get(), I.get())
-            #LLerr = GDK.gpu.LLerr.get()
-            #LLerrs = kern.GDKs.npy.LLerr
-            #na = np.isnan(LLerr)
-            #print('main cpu made nan', np.isnan(LLerrs).any())
-            #print('main gpu made nan', na.any(), na.sum())
-            #print('diff', LLerr-LLerrs)
-            #print('LLerr', LLerrs)
-            #print(I.get()[na])
-            #print(w.get()[na])
-            #print(GDK.gpu.LLerr.get()[0])
+            ev = cuda.Event()
+            ev.record(GDK.queue)
+
             GDK.error_reduce(addr, err_phot)
             BW(aux, aux)
-            #print(err_phot.get())
+
             use_atomics = self.p.object_update_cuda_atomics
             addr = prep.addr_gpu if use_atomics else prep.addr2_gpu
             POK.ob_update_ML(addr, obg, pr, aux, atomics=use_atomics)
@@ -391,14 +460,16 @@ class GaussianModel(BaseModelSerial):
             addr = prep.addr_gpu if use_atomics else prep.addr2_gpu
             POK.pr_update_ML(addr, prg, ob, aux, atomics=use_atomics)
 
-        # TODO we err_phot.sum, but not necessraily this error_dct until the end of contiguous iteration
+        # TODO we err_phot.sum, but not necessarily this error_dct until the end of contiguous iteration
         for dID, prep in self.engine.diff_info.items():
-            err_phot = prep.err_phot_gpu.get() / np.prod(prep.weights.shape)
+            err_phot = prep.err_phot_gpu.get()
+            LL += err_phot.sum()
+            err_phot /= np.prod(prep.weights.shape[-2:])
             err_fourier = np.zeros_like(err_phot)
             err_exit = np.zeros_like(err_phot)
             errs = np.ascontiguousarray(np.vstack([err_fourier, err_phot, err_exit]).T)
             error_dct.update(zip(prep.view_IDs, errs))
-            LL += err_phot.sum()
+
 
         # MPI reduction of gradients
 
@@ -427,7 +498,7 @@ class GaussianModel(BaseModelSerial):
                 LL += self.regularizer.LL
 
         self.LL = LL / self.tot_measpts
-
+        print(self.LL)
         return error_dct
 
     def poly_line_coeffs(self, c_ob_h, c_pr_h):

ptypy/engines/ML_serial.py
@@ -370,12 +370,13 @@ class GaussianModel(BaseModelSerial):
             POK.pr_update_ML(addr, prg, ob, aux)
 
         for dID, prep in self.engine.diff_info.items():
-            err_phot = prep.err_phot / np.prod(prep.weights.shape)
+            err_phot = prep.err_phot
+            LL += err_phot.sum()
+            err_phot /= np.prod(prep.weights.shape[-2:])
             err_fourier = np.zeros_like(err_phot)
             err_exit = np.zeros_like(err_phot)
             errs = np.ascontiguousarray(np.vstack([err_fourier, err_phot, err_exit]).T)
             error_dct.update(zip(prep.view_IDs, errs))
-            LL += err_phot.sum()
 
         # MPI reduction of gradients
         ob_grad.allreduce()
@@ -389,7 +390,7 @@ class GaussianModel(BaseModelSerial):
                 LL += self.regularizer.LL
 
         self.LL = LL / self.tot_measpts
-
+        print(self.LL)
         return error_dct
 
     def poly_line_coeffs(self, c_ob_h, c_pr_h):