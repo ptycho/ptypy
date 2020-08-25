a15bd6e971f6e2200dfdf6fffe6517acac6cf835
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2020-08-25 18:11:52 +0100

    fix name of engine in diamond benchmarks

--- benchmark/diamond_benchmarks/moonflower_scripts/insanity.py
+++ benchmark/diamond_benchmarks/moonflower_scripts/insanity.py
@@ -57,7 +57,7 @@ p.scans.insane.data.psf = 0.0
 # attach a reconstrucion engine
 p.engines = u.Param()
 p.engines.engine00 = u.Param()
-p.engines.engine00.name = 'DM_pycuda_stream'
+p.engines.engine00.name = 'DM_pycuda_streams'
 p.engines.engine00.numiter = 200
 p.engines.engine00.numiter_contiguous = 10
 p.engines.engine00.probe_update_start = 1
