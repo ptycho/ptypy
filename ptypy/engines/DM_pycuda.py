0c9ec7f322b29e13a25011d22fbbcb0e19a4b627
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2020-09-07 19:42:39 +0100

    Fixed bug in DM_pycuda

--- ptypy/engines/DM_pycuda.py
+++ ptypy/engines/DM_pycuda.py
@@ -346,7 +346,7 @@ class DM_pycuda(DM_serial.DM_serial):
         #     s.data[:] = s.gpu.get()
         for dID, prep in self.diff_info.items():
             err_fourier = prep.err_fourier_gpu.get()
-            err_phot = prep.err_phot.get()
+            err_phot = prep.err_phot_gpu.get()
             err_exit = prep.err_exit_gpu.get()
             errs = np.ascontiguousarray(np.vstack([err_fourier, err_phot, err_exit]).T)
             error.update(zip(prep.view_IDs, errs))
