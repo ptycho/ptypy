077c8a2fc1b8dd3eb220e18dbf3598fd16e8db70
Jorg Lotze <jorg.lotze@xcelerit.com>
2021-03-30 11:51:17 +0100

    Revert accidental commit: "cleanup and re-organising file locations"
    This reverts commit 6c4904b0a81194fc077befd28c472cf92297882a.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import distutils
-from setupext_nvidia import CustomBuildExt
+from ptypy.accelerate.cuda_pycuda.import_fft import CustomBuildExt
 import setuptools #, setuptools.command.build_ext
 from distutils.core import setup
 import os
@@ -65,12 +65,13 @@ if __name__ == '__main__':
     except:
         vers = VERSION
 
-# filtered Cuda FFT extension module
-cufft_dir = os.path.join('ptypy', 'accelerate', 'cuda_pycuda', 'cuda', 'filtered_fft')
+module_dir = os.path.join(__file__.strip('setup.py'), 
+    'ptypy', 'accelerate', 'cuda_pycuda', 'cuda', 'filtered_fft')
+
 ext_modules = [
     distutils.core.Extension("ptypy.filtered_cufft",
-        sources=[os.path.join(cufft_dir, "module.cpp"),
-                 os.path.join(cufft_dir, "filtered_fft.cu")]
+        sources=[os.path.join(module_dir, "module.cpp"),
+                 os.path.join(module_dir, "filtered_fft.cu")]
     )
 ]
 cmdclass = {"build_ext": CustomBuildExt}
@@ -87,7 +88,8 @@ setup(
     package_dir={'ptypy': 'ptypy'},
     packages=package_list,
     package_data={'ptypy': ['resources/*',],
-                  'ptypy.accelerate.cuda_pycuda.cuda': ['*.cu']},
+                  'ptypy.accelerate.cuda_pycuda.cuda': ['*.cu'],
+                  'ptypy.accelerate.cuda_pycuda.cuda.filtered_fft': ['*.hpp', '*.cpp', 'Makefile', '*.cu', '*.h']},
     scripts=['scripts/ptypy.plot',
              'scripts/ptypy.inspect',
              'scripts/ptypy.plotclient',
