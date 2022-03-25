af9cc8c6d005f23b06397c3edafdc397ba7549c0
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2022-03-25 14:02:49 +0000

    fixed log_likelihood.cu merge

--- ptypy/accelerate/cuda_pycuda/cuda/log_likelihood.cu
+++ ptypy/accelerate/cuda_pycuda/cuda/log_likelihood.cu
@@ -31,6 +31,151 @@ template <class AUX_T>
 inline __device__ void log_likelihood_impl(
                    int nmodes,
                    AUX_T *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int addr_stride = 15;
+
+  const int *ea = addr + 6 + (blockIdx.x * nmodes) * addr_stride;
+  const int *da = addr + 9 + (blockIdx.x * nmodes) * addr_stride;
+  const int *ma = addr + 12 + (blockIdx.x * nmodes) * addr_stride;
+
+  aux += ea[0] * A * B;
+  fmag += da[0] * A * B;
+  fmask += ma[0] * A * B;
+  llerr += da[0] * A * B;
+  MATH_TYPE norm = A * B;
+
+  for (int a = ty; a < A; a += blockDim.y)
+  {
+    for (int b = tx; b < B; b += blockDim.x)
+    {
+      MATH_TYPE acc = 0.0;
+      for (int idx = 0; idx < nmodes; ++idx)
+      {
+        acc += aux_intensity(aux[a * B + b + idx * A * B]);
+      }
+      auto I = MATH_TYPE(fmag[a * B + b]) * MATH_TYPE(fmag[a * B + b]);
+      llerr[a * B + b] =
+          MATH_TYPE(fmask[a * B + b]) * (acc - I) * (acc - I) / (I + 1) / norm;
+    }
+  }
+}
+
+// specify max number of threads/block and min number of blocks per SM,
+// to assist the compiler in register optimisations.
+// We achieve a higher occupancy in this case, as less registers are used
+// (guided by profiler)
+extern "C" __global__ void __launch_bounds__(1024, 2)
+    log_likelihood(int nmodes,
+                   complex<OUT_TYPE> *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  log_likelihood_impl(nmodes, aux, fmask, fmag, addr, llerr, A, B);
+}
+
+extern "C" __global__ void __launch_bounds__(1024, 2)
+    log_likelihood_auxintensity(int nmodes,
+                   OUT_TYPE *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  log_likelihood_impl(nmodes, aux, fmask, fmag, addr, llerr, A, B);
+}
+
+/////////////////// version with 1 thread block per x dimension only
+template <class AUX_T>
+__device__ inline void log_likelihood2_impl(
+                   int nmodes,
+                   AUX_T *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  int bid = blockIdx.z;
+  int tx = threadIdx.x;
+  int a = threadIdx.y + blockIdx.y * blockDim.y;
+  if (a >= A)
+    return;
+  int addr_stride = 15;
+
+  const int *ea = addr + 6 + (bid * nmodes) * addr_stride;
+  const int *da = addr + 9 + (bid * nmodes) * addr_stride;
+  const int *ma = addr + 12 + (bid * nmodes) * addr_stride;
+
+  aux += ea[0] * A * B;
+  fmag += da[0] * A * B;
+  fmask += ma[0] * A * B;
+  llerr += da[0] * A * B;
+  MATH_TYPE norm = A * B;
+
+  for (int b = tx; b < B; b += blockDim.x)
+  {
+    MATH_TYPE acc = 0.0;
+    for (int idx = 0; idx < nmodes; ++idx)
+    {
+      acc += aux_intensity(aux[a * B + b + idx * A * B]);
+    }
+    auto I = MATH_TYPE(fmag[a * B + b]) * MATH_TYPE(fmag[a * B + b]);
+    llerr[a * B + b] =
+        MATH_TYPE(fmask[a * B + b]) * (acc - I) * (acc - I) / (I + 1) / norm;
+  }
+}
+
+// ML variant which uses weights and intensity directly.
+// Based of log_likelihood
+extern "C" __global__ void __launch_bounds__(1024, 2)
+    log_likelihood2(int nmodes,
+                   complex<OUT_TYPE> *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  log_likelihood2_impl(nmodes, aux, fmask, fmag, addr, llerr, A, B);
+}
+
+extern "C" __global__ void 
+    log_likelihood2_auxintensity(int nmodes,
+                   OUT_TYPE *aux,
+                   const IN_TYPE *fmask,
+                   const IN_TYPE *fmag,
+                   const int *addr,
+                   IN_TYPE *llerr,
+                   int A,
+                   int B)
+{
+  log_likelihood2_impl(nmodes, aux, fmask, fmag, addr, llerr, A, B);
+}
+
+
+
+// ML variant which uses weights and intensity directly.
+// Based of log_likelihood
+template <class AUX_T>
+inline __device__ void log_likelihood_ml_impl(
+                   int nmodes,
+                   AUX_T *aux,
                    const IN_TYPE *weights,
                    const IN_TYPE *I,
                    const int *addr,
@@ -73,76 +218,6 @@ inline __device__ void log_likelihood_impl(
 // We achieve a higher occupancy in this case, as less registers are used
 // (guided by profiler)
 extern "C" __global__ void __launch_bounds__(1024, 2)
-    log_likelihood(int nmodes,
-                   complex<OUT_TYPE> *aux,
-                   const IN_TYPE *weights,
-                   const IN_TYPE *I,
-                   const int *addr,
-                   IN_TYPE *llerr,
-                   int A,
-                   int B)
-{
-  log_likelihood_impl(nmodes, aux, weights, I, addr, llerr, A, B);
-}
-
-extern "C" __global__ void __launch_bounds__(1024, 2)
-    log_likelihood_auxintensity(int nmodes,
-                   OUT_TYPE *aux,
-                   const IN_TYPE *weights,
-                   const IN_TYPE *I,
-                   const int *addr,
-                   IN_TYPE *llerr,
-                   int A,
-                   int B)
-{
-  log_likelihood_impl(nmodes, aux, weights, I, addr, llerr, A, B);
-}
-
-/////////////////// version with 1 thread block per x dimension only
-template <class AUX_T>
-__device__ inline void log_likelihood2_impl(
-                   int nmodes,
-                   AUX_T *aux,
-                   const IN_TYPE *weights,
-                   const IN_TYPE *I,
-                   const int *addr,
-                   IN_TYPE *llerr,
-                   int A,
-                   int B)
-{
-  int bid = blockIdx.z;
-  int tx = threadIdx.x;
-  int a = threadIdx.y + blockIdx.y * blockDim.y;
-  if (a >= A)
-    return;
-  int addr_stride = 15;
-
-  const int *ea = addr + 6 + (bid * nmodes) * addr_stride;
-  const int *da = addr + 9 + (bid * nmodes) * addr_stride;
-  const int *ma = addr + 12 + (bid * nmodes) * addr_stride;
-
-  aux += ea[0] * A * B;
-  weights += da[0] * A * B;
-  I += ma[0] * A * B;
-  llerr += da[0] * A * B;
-  MATH_TYPE norm = A * B;
-
-  for (int b = tx; b < B; b += blockDim.x)
-  {
-    MATH_TYPE acc = 0.0;
-    MATH_TYPE i = I[a * B + b];
-    for (int idx = 0; idx < nmodes; ++idx)
-    {
-      acc += aux_intensity(aux[a * B + b + idx * A * B]);
-    }
-    llerr[a * B + b] =
-        MATH_TYPE(weights[a * B + b]) * (acc - i) * (acc - i) / norm;
-  }
-}
-
-// ML variant which uses weights and intensity directly.
-// Based of log_likelihood
-extern "C" __global__ void __launch_bounds__(1024, 2)
     log_likelihood_ml(int nmodes,
                    complex<OUT_TYPE> *aux,
                    const IN_TYPE *weights,
@@ -152,11 +227,11 @@ extern "C" __global__ void __launch_bounds__(1024, 2)
                    int A,
                    int B)
 {
-  log_likelihood2_impl(nmodes, aux, weights, I, addr, llerr, A, B);
+  log_likelihood_ml_impl(nmodes, aux, weights, I, addr, llerr, A, B);
 }
 
-extern "C" __global__ void 
-    log_likelihood2_auxintensity(int nmodes,
+extern "C" __global__ void __launch_bounds__(1024, 2)
+    log_likelihood_ml_auxintensity(int nmodes,
                    OUT_TYPE *aux,
                    const IN_TYPE *weights,
                    const IN_TYPE *I,
@@ -165,5 +240,5 @@ extern "C" __global__ void
                    int A,
                    int B)
 {
-  log_likelihood2_impl(nmodes, aux, weights, I, addr, llerr, A, B);
+  log_likelihood_ml_impl(nmodes, aux, weights, I, addr, llerr, A, B);
 }
