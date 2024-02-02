5d852c9c63b74668c447d608e6eefb8a8daf968d
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-02-02 11:32:00 +0000

    Remove ob/pr avg wasp kernels

--- ptypy/accelerate/cuda_common/avg_wasp.cu
+++ ptypy/accelerate/cuda_common/avg_wasp.cu
@@ -1,4 +1,4 @@
-/** ob_avg_wasp
+/** avg_wasp
 *
 * Data types:
 * - IN_TYPE: the data type for the inputs (float or double)
@@ -13,35 +13,35 @@
 // We achieve a higher occupancy in this case, as less registers are used
 // (guided by profiler)
 extern "C" __global__ void __launch_bounds__(1024, 2)
-    ob_avg_wasp(complex<OUT_TYPE> *obj,
-                const complex<IN_TYPE>* __restrict__ obj_sum_nmr,
-                const IN_TYPE* __restrict__ obj_sum_dnm,
-                int A,
-                int B,
-                int C
-                )
+    avg_wasp(complex<OUT_TYPE> *arr,
+             const complex<IN_TYPE>* __restrict__ nmr,
+             const IN_TYPE* __restrict__ dnm,
+             int A,
+             int B,
+             int C
+             )
 {
     const int bid = blockIdx.z;
     const int tx = threadIdx.x;
     const int b = threadIdx.y + blockIdx.y * blockDim.y;
 
-    /*go to this object mode*/
-    obj += bid * B * C;
-    obj_sum_nmr += bid * B * C;
-    obj_sum_dnm += bid * B * C;
+    /*go to this mode*/
+    arr += bid * B * C;
+    nmr += bid * B * C;
+    dnm += bid * B * C;
 
     if (b >= B)
         return;
 
     for (int c = tx; c < C; c += blockDim.x) {
-      if (obj_sum_dnm[b * C + c] != 0) {
-        auto avg_val_tmp = obj_sum_nmr[b * C + c] / obj_sum_dnm[b * C + c];
+      if (dnm[b * C + c] != 0) {
+        auto avg_val_tmp = nmr[b * C + c] / dnm[b * C + c];
         complex<OUT_TYPE> avg_val = avg_val_tmp;
-        obj[b * C + c] = avg_val;
+        arr[b * C + c] = avg_val;
       }
       else {
-        complex<OUT_TYPE> avg_val = obj_sum_nmr[b * C + c];
-        obj[b * C + c] = avg_val;
+        complex<OUT_TYPE> avg_val = nmr[b * C + c];
+        arr[b * C + c] = avg_val;
       }
     }
 }
