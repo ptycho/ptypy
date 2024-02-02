cb888b750ddaed139366451e124ee1c05a9813ad
Timothy Poon <62692924+ptim0626@users.noreply.github.com>
2024-02-02 16:13:53 +0000

    Add WASP reconstruction engine (#522)
    * Add sign function to return z/|z| for complex number
    
    * Naive implementation of RASP
    
    * Rewrite the RASP algorithm
    
    [WIP] Fill bottom object sum with mean power
    
    Rewrite the RASP algorithm
    
    * Remove denominator fill-up (make recon a bit unstable)
    
    * Support MPI in RASP
    
    * Skip calculating probe max power (already present)
    
    * Delete helper containers as in the parent
    
    Remove containers
    
    * Implement RASP serial version
    
    [WIP] Looks like a working version
    
    Change base RASP inheritance to PositionCorrectEngine to reduce unnecessary operations
    
    [WIP] Fix RASP averaging (was not doing it at all)
    
    [WIP] Final working RASP serial
    
    * Add random seed (for shuffling diff) as a parameter in RASP
    
    * Ensure only one RNG is generated and input vieworder is the same everytime
    
    * Ensure same view order is used between RASP_serial and RASP
    
    * Use the same epsilon with normal implementation for float-point division
    
    * Use customised abs2 consistently when computing af
    
    * Avoid extra computation of af to prevent loss in precision
    
    * Use the exact formula with normal implementation to prevent FP arithmetic issue
    
    * Renmae rasp files to WASP
    
    * Revert "Add sign function to return z/|z| for complex number"
    
    This reverts commit f06b5816d256969fec47af26ec096325a032c6fb.
    
    * Revert "Avoid extra computation of af to prevent loss in precision"
    
    This reverts commit 90457da546a025fcc67762f151ee42cb5e3d774b.
    
    * Rename RASP to WASP
    
    * Update WASP publication
    
    * Add moonflower example for WASP_serial
    
    * Remove some irrelevant parameters in WASP
    
    * Add tests for WASP_serial
    
    * [WIP] Draft WASP pycuda
    
    * Divide WASP_serial ob pr update into two as in the normal version
    
    * [WIP] WASP pycuda
    
    * Step the ob and pr sum to the correct location for updating
    
    * Add the WASP ob and pr averaging kernels
    
    * Complete WASP_pycuda engine
    
    * Match serial version of WASP to its pycuda's for averaging ob/pr
    
    * Modify WASP examples for multiple number of probe modes
    
    * Add WASP pycuda example
    
    * Add WASP pycuda tests
    
    * Remove unsed ob_abs2 in WASP pr update kernel
    
    * Add WASP cupy kernel calls
    
    * Draft WASP cupy engine
    
    * Handle mulit-probe abs2 in the kernel and pycuda version
    
    * Add tests for WASP pycuda kernels
    
    * Fix WASP cupy ob_update kernel by making sure input is C-contiguous
    
    * Add WASP cupy kernel tests
    
    * Revert "Use the same epsilon with normal implementation for float-point division"
    
    This reverts commit 2e91841d47ef26d60d35ea9f06b089923cbe1e3a.
    
    * Revert "Use customised abs2 consistently when computing af"
    
    This reverts commit b9c158d00e49e49b4f336717c4a13074fb9d97e5.
    
    * Revert "Use the exact formula with normal implementation to prevent FP arithmetic issue"
    
    This reverts commit 753f154f17b2f16e154104ab09910c5823ffd324.
    
    * Add probe centering to WASP pycuda and cupy
    
    * Remove custom probe power correction
    
    * Remove tests related to custom probe power correction
    
    * Add tests for WASP cupy
    
    * Add WASP cupy moonflower example
    
    * Remove unnecessary clean-up in WASP pycuda because of #520
    
    * Combine ob/pr avg wasp kernel in serial version
    
    * Remove ob/pr avg wasp kernels
    
    * Combine ob/pr avg wasp kernel in pycuda version
    
    * Combine ob/pr avg wasp kernels in cupy version
    
    * Remove redundant ob/pr avg wasp kernel tests

---
+++ templates/misc/moonflower_WASP_cupy.py
@@ -0,0 +1,59 @@
+"""
+This script is a test for ptychographic reconstruction in the absence
+of actual data. It uses the test Scan class
+`ptypy.core.data.MoonFlowerScan` to provide "data".
+"""
+from ptypy.core import Ptycho
+from ptypy import utils as u
+from ptypy.custom import WASP_cupy
+import numpy as np
+
+import tempfile
+tmpdir = tempfile.gettempdir()
+
+p = u.Param()
+
+# for verbose output
+p.verbose_level = "info"
+p.frames_per_block = 200
+
+# set home path
+p.io = u.Param()
+p.io.home = "/".join([tmpdir, "ptypy"])
+p.io.autosave = u.Param(active=False)
+p.io.autoplot = u.Param(active=False)
+p.io.interaction = u.Param(active=False)
+
+# max 200 frames (128x128px) of diffraction data
+p.scans = u.Param()
+p.scans.MF = u.Param()
+# now you have to specify which ScanModel to use with scans.XX.name,
+# just as you have to give 'name' for engines and PtyScan subclasses.
+p.scans.MF.name = 'BlockFull'
+p.scans.MF.data= u.Param()
+p.scans.MF.data.name = 'MoonFlowerScan'
+p.scans.MF.data.shape = 128
+p.scans.MF.data.num_frames = 200
+p.scans.MF.data.save = None
+
+# position distance in fraction of illumination frame
+p.scans.MF.data.density = 0.2
+# total number of photon in empty beam
+p.scans.MF.data.photons = 1e8
+# Gaussian FWHM of possible detector blurring
+p.scans.MF.data.psf = 0.
+
+# number of probe modes
+p.scans.MF.illumination=u.Param(diversity=None)
+p.scans.MF.coherence = u.Param(num_probe_modes=2)
+
+# attach a reconstrucion engine
+p.engines = u.Param()
+p.engines.engine00 = u.Param()
+p.engines.engine00.name = 'WASP_cupy'
+p.engines.engine00.numiter = 80
+p.engines.engine00.random_seed = 721
+
+# prepare and run
+if __name__ == "__main__":
+    P = Ptycho(p,level=5)
---
+++ test/accelerate_tests/cuda_pycuda_tests/WASP_tests.py
@@ -0,0 +1,154 @@
+"""
+Test for the WASP engine.
+
+This file is part of the PTYPY package.
+    :copyright: Copyright 2014 by the PTYPY team, see AUTHORS.
+    :license: see LICENSE for details.
+"""
+import tempfile
+import shutil
+import unittest
+
+import numpy as np
+
+from test import utils as tu
+from ptypy import utils as u
+from ptypy.custom import WASP_serial, WASP_pycuda
+from ptypy.utils import parallel
+
+
+class WASPPycudaTest(unittest.TestCase):
+
+    def setUp(self):
+        self.outpath = tempfile.mkdtemp(suffix="WASP_pycuda_test")
+
+    def tearDown(self):
+        shutil.rmtree(self.outpath)
+
+    def check_engine_output(self, output, plotting=False, debug=False):
+        P_WASP_serial, P_WASP_pycuda = output
+        numiter = len(P_WASP_serial.runtime["iter_info"])
+        LL_WASP_serial = np.array([P_WASP_serial.runtime["iter_info"][i]["error"][1] for i in range(numiter)])
+        LL_WASP_pycuda = np.array([P_WASP_pycuda.runtime["iter_info"][i]["error"][1] for i in range(numiter)])
+        crop = 42
+        OBJ_WASP_pycuda, OBJ_WASP_serial = P_WASP_pycuda.obj.S["SMFG00"].data[0,crop:-crop,crop:-crop], P_WASP_serial.obj.S["SMFG00"].data[0,crop:-crop,crop:-crop]
+        PRB_WASP_pycuda, PRB_WASP_serial = P_WASP_pycuda.probe.S["SMFG00"].data[0], P_WASP_serial.probe.S["SMFG00"].data[0]
+        eng_WASP_serial = P_WASP_serial.engines["engine00"]
+        eng_WASP_pycuda = P_WASP_pycuda.engines["engine00"]
+        if debug:
+            import matplotlib.pyplot as plt
+            plt.figure("WASP serial debug")
+            plt.imshow(np.abs(eng_WASP_serial.debug))
+            plt.figure("WASP pycuda debug")
+            plt.imshow(np.abs(eng_WASP_pycuda.debug))
+            plt.show()
+
+        if plotting:
+            import matplotlib.pyplot as plt
+            plt.figure("Errors")
+            plt.plot(LL_WASP_serial, label="WASP_serial")
+            plt.plot(LL_WASP_pycuda, label="WASP_pycuda")
+            plt.legend()
+            plt.show()
+            plt.figure("Phase WASP serial")
+            plt.imshow(np.angle(OBJ_WASP_serial))
+            plt.figure("Ampltitude WASP serial")
+            plt.imshow(np.abs(OBJ_WASP_serial))
+            plt.figure("Phase WASP pycuda")
+            plt.imshow(np.angle(OBJ_WASP_pycuda))
+            plt.figure("Amplitude WASP pycuda")
+            plt.imshow(np.abs(OBJ_WASP_pycuda))
+            plt.figure("Phase difference")
+            plt.imshow(np.angle(OBJ_WASP_pycuda) - np.angle(OBJ_WASP_serial), vmin=-0.1, vmax=0.1)
+            plt.colorbar()
+            plt.figure("Amplitude difference")
+            plt.imshow(np.abs(OBJ_WASP_pycuda) - np.abs(OBJ_WASP_serial), vmin=-0.1, vmax=0.1)
+            plt.colorbar()
+            plt.show()
+
+            plt.figure("Phase WASP serial")
+            plt.imshow(np.angle(PRB_WASP_serial))
+            plt.figure("Ampltitude WASP serial")
+            plt.imshow(np.abs(PRB_WASP_serial))
+            plt.figure("Phase WASP pycuda")
+            plt.imshow(np.angle(PRB_WASP_pycuda))
+            plt.figure("Amplitude WASP pycuda")
+            plt.imshow(np.abs(PRB_WASP_pycuda))
+            plt.figure("Phase difference")
+            plt.imshow(np.angle(PRB_WASP_pycuda) - np.angle(PRB_WASP_serial), vmin=-0.1, vmax=0.1)
+            plt.colorbar()
+            plt.figure("Amplitude difference")
+            plt.imshow(np.abs(PRB_WASP_pycuda) - np.abs(PRB_WASP_serial), vmin=-0.1, vmax=0.1)
+            plt.colorbar()
+            plt.show()
+
+        RMSE_ob = (np.mean(np.abs(OBJ_WASP_pycuda - OBJ_WASP_serial)**2))
+        RMSE_pr = (np.mean(np.abs(PRB_WASP_pycuda - PRB_WASP_serial)**2))
+        np.testing.assert_allclose(RMSE_ob, 0.0, atol=1e-1,
+                                    err_msg="The object arrays are not matching as expected")
+
+        # the extremly high tolerance for probe is a result of precision
+        # difference between the serial and pycuda version
+        np.testing.assert_allclose(RMSE_pr, 0.0, atol=1e3,
+                                    err_msg="The probe arrays are not matching as expected")
+
+    def test_WASP_pycuda_base(self):
+        out = []
+        for eng in ["WASP_serial", "WASP_pycuda"]:
+            engine_params = u.Param()
+            engine_params.name = eng
+            engine_params.numiter = 10
+            engine_params.random_seed = 721
+            out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
+                                           scanmodel="BlockFull", autosave=False, verbose_level="critical"))
+
+        if parallel.master:
+            self.check_engine_output(out, plotting=False, debug=False)
+
+    def test_WASP_pycuda_clip(self):
+        out = []
+        for eng in ["WASP_serial", "WASP_pycuda"]:
+            engine_params = u.Param()
+            engine_params.name = eng
+            engine_params.numiter = 10
+            engine_params.clip_object = (0, 2)
+            engine_params.random_seed = 721
+            out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
+                                           scanmodel="BlockFull", autosave=False, verbose_level="critical"))
+
+        if parallel.master:
+            self.check_engine_output(out, plotting=False, debug=False)
+
+    def test_WASP_pycuda_alpha_beta(self):
+        out = []
+        for eng in ["WASP_serial", "WASP_pycuda"]:
+            engine_params = u.Param()
+            engine_params.name = eng
+            engine_params.numiter = 10
+            engine_params.alpha = 0.64
+            engine_params.beta = 0.94
+            engine_params.random_seed = 721
+            out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
+                                           scanmodel="BlockFull", autosave=False, verbose_level="critical"))
+
+        if parallel.master:
+            self.check_engine_output(out, plotting=False, debug=False)
+
+    def test_WASP_pycuda_all(self):
+        out = []
+        for eng in ["WASP_serial", "WASP_pycuda"]:
+            engine_params = u.Param()
+            engine_params.name = eng
+            engine_params.numiter = 10
+            engine_params.clip_object = (0, 2)
+            engine_params.alpha = 0.64
+            engine_params.beta = 0.94
+            engine_params.random_seed = 721
+            out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
+                                           scanmodel="BlockFull", autosave=False, verbose_level="critical"))
+
+        if parallel.master:
+            self.check_engine_output(out, plotting=False, debug=False)
+
+if __name__ == "__main__":
+    unittest.main()
