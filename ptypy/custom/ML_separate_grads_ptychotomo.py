7c319b60f3fc4327f38002ffe51938d9062250be
Jari <jaroslav.fowkes@stfc.ac.uk>
2024-07-01 15:27:04 +0100

    Tidy plotting

--- ptypy/custom/ML_separate_grads_ptychotomo.py
+++ ptypy/custom/ML_separate_grads_ptychotomo.py
@@ -534,20 +534,15 @@ class MLPtychoTomo(PositionCorrectionEngine):
             self.pr += self.pr_h
 
             # FIXME: move saving volumes to run script
-            try:
-                iter = self.ptycho.runtime.iter_info[-1]['iteration']
-            except:
-                iter = 0
-
-            # if iter==200:
-            #     self.projector.plot_vol(self.rho, title= '200iters')
-
-            # Saving volumes when running on Wilson
-            if iter==200 and not os.path.exists("/dls/science/users/iat69393/ptycho-tomo-project/recon_vol_phase_HARDC_it{}.cmap".format(iter)):
-                with h5py.File("/dls/science/users/iat69393/ptycho-tomo-project/SMALLER_recon_vol_ampl_HARDC_it{}.cmap".format(iter), "w") as f:
-                    f["data"] = np.imag(self.rho)[100:-100,100:-100,100:-100]
-                with h5py.File("/dls/science/users/iat69393/ptycho-tomo-project/SMALLER_NEG_recon_vol_phase_HARDC_it{}.cmap".format(iter), "w") as f:
-                    f["data"] = -np.real(self.rho)[100:-100,100:-100,100:-100]
+            # Saving volumes when running Toy Problem (saves to png)
+            if self.curiter == 199: # curiter starts at zero
+                self.projector.plot_vol(self.rho, title= '200iters')
+            # Saving volumes when running Real Data (saves to cmap)
+            #if self.curiter==199 and not os.path.exists("/dls/science/users/iat69393/ptycho-tomo-project/recon_vol_phase_HARDC_it{}.cmap".format(iter)):
+            #    with h5py.File("/dls/science/users/iat69393/ptycho-tomo-project/SMALLER_recon_vol_ampl_HARDC_it{}.cmap".format(iter), "w") as f:
+            #        f["data"] = np.imag(self.rho)[100:-100,100:-100,100:-100]
+            #    with h5py.File("/dls/science/users/iat69393/ptycho-tomo-project/SMALLER_NEG_recon_vol_phase_HARDC_it{}.cmap".format(iter), "w") as f:
+            #        f["data"] = -np.real(self.rho)[100:-100,100:-100,100:-100]
             # FIXME: end move saving volumes to run script
 
             # Position correction
