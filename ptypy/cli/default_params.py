ae9911d7b2bdb78c1f437af29af740055ac1b594
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2024-07-30 16:35:09 +0100

    Refactor and simplify scripts

--- ptypy/cli/default_params.py
+++ ptypy/cli/default_params.py
@@ -2,15 +2,33 @@
 
 from ptypy import utils as u
 import textwrap
+import argparse
 
+def ptypy_csv2cp():
+    opt = parse()
+    convert_and_print(opt)
+
+def parse():
+    parser = argparse.ArgumentParser(description='Dump default parameter in ConfigParser format.')
+    parser.add_argument('-p', '--path', dest='path', type=str, default=None,
+                   help='A valid path in the parameter structure')
+    parser.add_argument('-d', '--depth', dest='depth', type=int, default=1,
+                   help='Recursion depth')
+    pars = parser.parse_args()
+    return pars
+
+def convert_and_print(pars):
+    root = u.validator.pdroot
+    if pars.path:
+        for node in pars.path.split('.'):
+            root = root.children[node]
+    print_param(root, depth=pars.depth)
 
 def write(x):
     print(x)
 
-
 def wrapdoc(x):
     return '\n  '.join(['\n  '.join(textwrap.wrap(line, 90, break_long_words=False, replace_whitespace=False)) for line in x.splitlines() if line.strip() != ''])
-
 
 def print_param(entry, parent=None, depth=50):
     if parent is not None:
@@ -24,9 +42,12 @@ def print_param(entry, parent=None, depth=50):
         write('doc = ' + wrapdoc(entry.longdoc))
         write('type = ' + ', '.join(entry.type))
         write('userlevel = %s' % entry.userlevel)
-        if entry.choices: write('choices = ' + ', '.join([str(x) for x in entry.choices]))
-        if entry.lowlim: write('lowlim = %s' % entry.lowlim)
-        if entry.uplim: write('uplim = %s' % entry.uplim)
+        if entry.choices: 
+            write('choices = ' + ', '.join([str(x) for x in entry.choices]))
+        if entry.lowlim: 
+            write('lowlim = %s' % entry.lowlim)
+        if entry.uplim: 
+            write('uplim = %s' % entry.uplim)
         write('')
     else:
         entry_name = ''
@@ -36,17 +57,4 @@ def print_param(entry, parent=None, depth=50):
     return
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Dump default parameter in ConfigParser format.')
-    parser.add_argument('-p', '--path', dest='path', type=str, default=None,
-                   help='A valid path in the parameter structure')
-    parser.add_argument('-d', '--depth', dest='depth', type=int, default=1,
-                   help='Recursion depth')
-    pars = parser.parse_args()
-
-    root = u.validator.pdroot
-    if pars.path:
-        for node in pars.path.split('.'):
-            root = root.children[node]
-
-    print_param(root, depth=pars.depth)
+    ptypy_csv2cp()
