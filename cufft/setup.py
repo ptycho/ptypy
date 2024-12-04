4be7bc14307be1cf55e3ee735840578cdb72265a
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-12-04 17:25:42 +0000

    Add back include

--- cufft/setup.py
+++ cufft/setup.py
@@ -5,7 +5,7 @@ import setuptools
 from distutils.core import setup, Extension
 import os
 # import pybind11
-# import sysconfig
+import sysconfig
 
 ext_modules = []
 cmdclass = {}
@@ -22,10 +22,10 @@ ext_modules.append(
     Extension("filtered_cufft",
         sources=[os.path.join(cufft_dir, "module.cpp"),
                 os.path.join(cufft_dir, "filtered_fft.cu")],
-        # include_dirs=[pybind11.get_include(),
-                      # sysconfig.get_paths()['include'],
-                      # ],
-        include_dirs=[ext_include_dir],
+        include_dirs=[ext_include_dir,
+                      sysconfig.get_paths()['include'],
+                      ],
+        # include_dirs=[ext_include_dir],
     )
 )
 cmdclass = {"build_ext": CustomBuildExt}
