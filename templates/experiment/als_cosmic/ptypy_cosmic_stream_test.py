cc0b0ebf922e156470f0889f3037bbd57311f8ac
Sam Welborn <swelborn@lbl.gov>
2025-02-06 10:07:46 -0500

    adjustments to streaming parameters

--- templates/experiment/als_cosmic/ptypy_cosmic_stream_test.py
+++ templates/experiment/als_cosmic/ptypy_cosmic_stream_test.py
@@ -12,7 +12,7 @@ import tempfile
 tmpdir = tempfile.gettempdir()
 
 p = u.Param()
-p.verbose_level = "info"
+p.verbose_level = "error"
 p.io = u.Param()
 p.io.home = "/".join([tmpdir, "ptypy"])
 
@@ -24,7 +24,7 @@ p.scans.MF.data.name = 'CosmicStreamLoader'
 p.scans.MF.data.source = 'localhost'
 p.scans.MF.data.dfile = None
 p.scans.MF.data.save = None
-p.scans.MF.data.min_frames=50
+p.scans.MF.data.min_frames=10
 p.scans.MF.data.rebin=2
 p.scans.MF.data.shape=512
 p.scans.MF.data.center=None
@@ -38,13 +38,14 @@ p.scans.MF.illumination.aperture.size = 0.0001
 p.scans.MF.illumination.aperture.central_stop = .2
 p.scans.MF.illumination.propagation = u.Param()
 p.scans.MF.illumination.propagation.focussed = 0.005
-p.scans.MF.illumination.propagation.parallel = -0.00005
+p.scans.MF.illumination.propagation.parallel = 0.00005
 p.scans.MF.illumination.propagation.spot_size = None
 
 p.engines = u.Param()
 p.engines.engine00 = u.Param()
 p.engines.engine00.name = 'DM'
 p.engines.engine00.numiter = 200
+p.engines.engine00.probe_fourier_support = 0.06
 #p.engines.engine01 = u.Param()
 #p.engines.engine01.name = 'ML'
 #p.engines.engine01.numiter = 20
