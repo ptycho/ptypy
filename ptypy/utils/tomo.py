4c9313ffba837319a0245d15d8222f8e27d68ec3
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2025-03-10 15:04:13 +0000

    Improve docstrings in tomo.py

--- ptypy/utils/tomo.py
+++ ptypy/utils/tomo.py
@@ -44,13 +44,20 @@ class AstraViewBased:
     def __init__(self, vol, n_views, view_shape, block_size, angles, shifts, view_to_proj_vectors):
 
         """
-        vol                     3d array
-        n_views                 number of views overall, considering all blocks
+        Receives: 
+        vol                     3D array representing the volume
+        n_views                 number of views overall, across all blocks
         view_shape              shape of view
-        block_size              size of chunk (a sub-part or equal to n_views) -- how many views you want to keep in memory
-        angles                  1d array of angles (same length as n_views)
-        shifts                  2d array of x-y shifts (same length as n_views, while other direction has len 2, i.e. x and y)
-        view_to_proj_vectors    2d array of vectors computed as differences between the center of views and the center of projections (same length as n_views)
+        block_size              size of chunk (a sub-part or equal to n_views)
+        angles                  1D array of angles (same length as n_views)
+        shifts                  2D array of x and y shifts, having shape: n_views x 2 (with 2 being x and y)
+        view_to_proj_vectors    2D array of vectors, computed as differences between the center of views and 
+                                the center of projections (same length as n_views)
+
+        Does the fllowing:
+         - the astra geometry for the projection array and the volume
+         - the astra ids for real and imag parts of both proj array and volume.
+         - sets up the configuration for both backward and forward 
         """
 
         self._n_views = n_views
@@ -95,7 +102,7 @@ class AstraViewBased:
     @property
     def proj_array(self):
         """
-        Has length "block_size" -- could be full list of views or smaller block.
+        Returns proj_array.
         """
         return self._proj_array
 
@@ -112,9 +119,8 @@ class AstraViewBased:
     @property
     def ind_of_views(self):
         """
-        Indices of views in the block. 
-        Has length of proj_array and so of "block_size".
-        These need to be ordered and correspond to proj_array.
+        Returns indexes of views in the block. 
+        Has length of "proj_array" and so of "block_size".
         """
         return self._ind_of_views
 
@@ -133,14 +139,14 @@ class AstraViewBased:
     @property
     def vol(self):
         """
-        Gets the volume
+        Returns the volume.
         """
         return self._vol
 
     @vol.setter
     def vol(self, vol):
         """
-        Sets the volume and updates data at id
+        Sets the volume and updates data at astra id.
         """   
         self._vol = vol
 
@@ -150,47 +156,59 @@ class AstraViewBased:
 
     def _update_data_at_id_astra(self, id, data):
         """
-        Helper function that updates data at a certain id
+        Helper function that updates data at a certain astra id.
         """
         astra.data3d.store(id, data)
 
     def _update_geom_at_id_astra(self, id, new_geom):
         """
-        Helper function that updates geometry at a certain id
+        Helper function that updates geometry at a certain astra id.
+        Receives an astra geometry, e.g. the one that "_create_proj_geom"
+        stores in "self._proj_geom"
         """
         astra.data3d.change_geometry(id, new_geom)
 
 
     def _create_proj_array_ids(self):
         """
-        Creates proj array ids.
-        _create_geom needs to be called before this. 
+        Creates astra ids for proj_array (real and imag).
+        _create_geom must be called before this. 
         This function is called once only, when the class is initialised.        
         """
-        self._proj_id_real = astra.data3d.create("-sino", self._proj_geom, self._proj_array.real)
-        self._proj_id_imag = astra.data3d.create("-sino", self._proj_geom, self._proj_array.imag)
+        self._proj_id_real = astra.data3d.create(
+            "-sino", self._proj_geom, self._proj_array.real
+            )
+        self._proj_id_imag = astra.data3d.create(
+            "-sino", self._proj_geom, self._proj_array.imag
+            )
 
 
     def _create_vol_ids(self):
         """
-        Creates vol ids.
-        _create_vol_geom needs to be called before this. 
+        Creates astra ids for volume (real and imag).
+        _create_vol_geom must be called before this. 
         This function is called once only, when the class is initialised.        
         """
-        self._vol_id_real = astra.data3d.create("-vol", self._vol_geom, self._vol.real)
-        self._vol_id_imag = astra.data3d.create("-vol", self._vol_geom, self._vol.imag)
+        self._vol_id_real = astra.data3d.create(
+            "-vol", self._vol_geom, self._vol.real
+            )
+        self._vol_id_imag = astra.data3d.create(
+            "-vol", self._vol_geom, self._vol.imag
+            )
 
 
     def _create_vol_geom(self):
         """Creates volume geometry"""
-        self._vol_geom = astra.create_vol_geom(self._vol.shape[0], self._vol.shape[1], self._vol.shape[2])
+        self._vol_geom = astra.create_vol_geom(
+            self._vol.shape[0], self._vol.shape[1], self._vol.shape[2]
+            )
 
 
     def _create_vector_for_proj_geom(self):
         """
-        Creates vector needed to construct proj array geometry
+        Creates vector needed to construct the astra proj array geometry.
         """
-        self._vec = np.zeros((self._n_views,12)) # size 1444x12, even with mpi
+        self._vec = np.zeros((self._n_views,12)) 
         for i in range(self._n_views):
 
             y = self._view_to_proj_vectors[i, 0] 
@@ -231,7 +249,7 @@ class AstraViewBased:
 
     def _setup_config_for_forward(self, type="FP3D_CUDA"):
         """
-        Gets the type, e.g. FP3D_CUDA, and prepares cfg
+        Gets the type, e.g. FP3D_CUDA, and prepares cfg.
         """
         cfg = astra.astra_dict(type)
         cfg["VolumeDataId"] = self._vol_id_real
@@ -245,7 +263,7 @@ class AstraViewBased:
         
     def _setup_config_for_backward(self, type="BP3D_CUDA"):
         """
-        Gets the type, e.g. FP3D_CUDA, and prepares cfg
+        Gets the type, e.g. FP3D_CUDA, and prepares cfg.
         """
         cfg = astra.astra_dict(type)
         cfg["ReconstructionDataId"] = self._vol_id_real
@@ -260,8 +278,8 @@ class AstraViewBased:
 
     def forward(self, iter=1, out=None):
         """
-        Computes forward projection, based on sel._vol (this must have been defined).
-        Places output in "out" if provided
+        Computes forward projection, based on self._vol (this must have 
+        been defined). Places output in "out" if provided.
         """
         astra.algorithm.run(self.forward_alg_id_real, iter)
         astra.algorithm.run(self.forward_alg_id_imag, iter)
@@ -286,8 +304,8 @@ class AstraViewBased:
 
     def backward(self, iter=1, out=None):
         """
-        Computes backward projection, based on _proj_array (this must have been defined).
-        Places output in "out" if provided.
+        Computes backward projection, based on _proj_array (this must have 
+        been defined). Places output in "out" if provided.
         """
         astra.algorithm.run(self.backward_alg_id_real, iter)
         astra.algorithm.run(self.backward_alg_id_imag, iter)
