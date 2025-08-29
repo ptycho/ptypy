1915474aab7b8c3c70c4804da7520f087465b854
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2025-08-29 10:47:36 +0000

    Crop volumes

--- compute_fourier_corr.py
+++ compute_fourier_corr.py
@@ -1,9 +1,9 @@
 from ptypy.utils.metrics import fouriershellcorrelation, fourierringcorrelation, frc_plot
 import numpy as np
 
-g_truth = np.load("/home/kpv14943/ptypy/rmap_242_glass.npy")[30:-30, 60:-60, 60:-60]
-vol_joint = np.load("/home/kpv14943/ptypy/vol_200iters_subs_half.npy")[30:-30, 60:-60, 60:-60]
-vol_conv = np.load("/home/kpv14943/ptypy/overall_vol_conv_REAL_400_subsampled_half.npy")[30:-30, 60:-60, 60:-60]
+g_truth = np.load("/home/kpv14943/ptypy/rmap_242_glass.npy")[50:-20, 70:-70, 70:-70]
+vol_joint = np.load("/home/kpv14943/ptypy/vol_200iters_subs_half.npy")[50:-20, 70:-70, 70:-70]
+vol_conv = np.load("/home/kpv14943/ptypy/overall_vol_conv_REAL_400_subsampled_half.npy")[50:-20, 70:-70, 70:-70]
 
 #FSC, T, fn = fourierringcorrelation(np.real(g_truth)[:, 100, :], np.real(vol_joint)[:, 100, :])
 FSC, T, fn = fouriershellcorrelation(np.real(g_truth), np.real(vol_joint))
