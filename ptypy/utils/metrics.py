dfcd35d679725fde26b08caa65d7c4b7c628830a
jcesardasilva <jcesardasilvaclean@gmail.com>
2025-09-17 09:56:59 +0200

    second iteration on the FSC/FRC with separated functions and shorter

--- ptypy/utils/metrics.py
+++ ptypy/utils/metrics.py
@@ -12,11 +12,12 @@ import numpy as np
 from scipy.fft import fftshift, ifftshift
 from scipy.ndimage import fourier_shift, gaussian_filter
 from skimage.registration import phase_cross_correlation
+import warnings
 
-__all__ = ['nyquist', 'ringthickness', 'apodization', 'fourierringcorrelation',
-            'fouriershellcorrelation', 'imgregistration','frc_plot']
+__all__ = ['nyquist', 'ringthickness', 'apodization', 'frc',
+            'fsc', 'imgregistration', 'fouriercorr_plot']
             
-def fourierringcorrelation(input1, input2, apod_width = 1, ringthick=1, threshold = 'onebit'):
+def frc(input1, input2, apod_width = 1, ringthick=1, threshold = 'onebit'):
     """
     Routine to compute the FRC
     
@@ -71,17 +72,83 @@ def fourierringcorrelation(input1, input2, apod_width = 1, ringthick=1, threshol
     img1_apod = input1 * window
     img2_apod = input2 * window
     
-    # # plotting 
-#     fig1 = plt.figure(1)
-#     ax1 = fig1.add_subplot(121)
-#     ax2 = fig1.add_subplot(122)
-#     im1 = ax1.imshow(img1_apod, cmap="bone", interpolation="none")
-#     ax1.set_title("image1")
-#     ax1.set_axis_off()
-#     im2 = ax2.imshow(img2_apod, cmap="bone", interpolation="none")
-#     ax2.set_title("image2")
-#     ax2.set_axis_off()
+    # calculating the Fourier correlation
+    FRC, T, fn = _fouriercorrelation(img1_apod, img2_apod, apod_width, ringthick, threshold)
 
+    return FRC, T, fn
+    
+def fsc(input1, input2, apod_width = 1, ringthick=1, threshold = 'onebit', apod_type='transaxial'):
+    """
+    Routine to compute the FSC
+    
+    Parameters
+    ----------
+    input1 :  array-like
+        array containing the first image, must be three-dimensional
+    
+    input2 : array-like
+        array containing the second image, must be three-dimensional
+    
+    apod_width : array-like
+        width of the apodization margin
+    
+    ringthick : int, optional
+        thickness of the ring for averaging the correlation
+    
+    threshold : str, optional
+        The option `onebit` means 1 bit threshold with ``SNRt = 0.5``, which
+        should be used for two independent measurements. The option `halfbit`
+        means 1/2 bit threshold with ``SNRt = 0.2071``, which should be
+        use for split tomogram. The default option is ``half-bit``.
+    
+    apod_type : str, optional
+        Type of apodization for 3D images. There are two types:
+        `transverse` and `transaxial`.
+    Returns
+    -------
+    FRC : array-like
+        1D array containing the FRC values
+    
+    T : array-like
+        1D array containing the 1-bit threshold
+    
+    fn : array-like
+        1D array containing the normalized frequencies
+    
+    """
+    # Check if the arrays have 2 dimensions
+    if input1.ndim==3 and input2.ndim==3:
+        ns, nr, nc = input1.shape
+    else:
+        raise ValueError("The arrays must have 3 dimensions")
+    # Check if the arrays have the same size
+    if input1.shape != input2.shape:
+        raise ValueError("The arrays must have the same size")
+        
+    # image registration
+    # need to align the two image
+    warnings.warn("For now, the alignment of 3D images has not been implemented yet!")
+    
+    # Apodization of the borders    
+    if apod_width == 0:
+        window = 1
+    else:
+        print("Apodization in 3D. This takes time and memory...")
+        window = apodization(input1, apod_width, apod_type)
+
+    # sagital slices
+    img1_apod = (window * input1)
+    img2_apod = (window * input2)
+    
+    # calculating the Fourier correlation
+    FSC, T, fn = _fouriercorrelation(img1_apod, img2_apod, apod_width, ringthick, threshold)
+
+    return FSC, T, fn
+    
+def _fouriercorrelation(input1, input2, apod_width = 1, ringthick=1, threshold = 'onebit'):
+    """
+    Auxiliary function for the calculation of either FRC or FSC
+    """
     # Setting the threshold
     if threshold == 'halfbit' or threshold == 'half-bit':
         print('Computing FRC using half-bit threshold')
@@ -96,11 +163,13 @@ def fourierringcorrelation(input1, input2, apod_width = 1, ringthick=1, threshol
         )
 
     # Computation of the FFTs
-    F1 = np.fft.fft2(np.fft.ifftshift(img1_apod)) # FFT of input1
-    F2 = np.fft.fft2(np.fft.ifftshift(img2_apod)) # FFT of input2
+    F1 = np.fft.fft2(np.fft.ifftshift(input1)) # FFT of input1
+    F2 = np.fft.fft2(np.fft.ifftshift(input2)) # FFT of input2
     
     # normalized frequencies
-    f,fnyquist = nyquist((nr,nc)) # Frequencies and Nyquist frequency
+    # Obtain the shape of the images
+    sh = np.asarray(input1.shape)
+    f,fnyquist = nyquist(sh) # Frequencies and Nyquist frequency
     fn = f/fnyquist
     
     # initializing variables
@@ -139,7 +208,7 @@ def fourierringcorrelation(input1, input2, apod_width = 1, ringthick=1, threshol
         npts[ii] = auxF1.shape[0]
 
     # The correlation
-    FRC = C / (np.sqrt(C1 * C2))
+    FC = C / (np.sqrt(C1 * C2))
 
     # The computation of the threshold
     Tnum = (
@@ -154,160 +223,12 @@ def fourierringcorrelation(input1, input2, apod_width = 1, ringthick=1, threshol
     )
     # The threshold
     T = Tnum / Tden
+    
+    print('Done')
 
-    return FRC, T, fn
-    
-def fouriershellcorrelation(input1, input2, apod_width = 1, ringthick=1, threshold = 'onebit'):
-    """
-    Routine to compute the FRC
-    
-    Parameters
-    ----------
-    input1 :  array-like
-        array containing the first image, must be three-dimensional
-    
-    input2 : array-like
-        array containing the second image, must be three-dimensional
-    
-    apod_width : array-like
-        width of the apodization margin
-    
-    ringthick : int, optional
-        thickness of the ring for averaging the correlation
-    
-    threshold : str, optional
-        The option `onebit` means 1 bit threshold with ``SNRt = 0.5``, which
-        should be used for two independent measurements. The option `halfbit`
-        means 1/2 bit threshold with ``SNRt = 0.2071``, which should be
-        use for split tomogram. The default option is ``half-bit``.
+    return FC, T, fn
 
-    Returns
-    -------
-    FRC : array-like
-        1D array containing the FRC values
-    
-    T : array-like
-        1D array containing the 1-bit threshold
-    
-    fn : array-like
-        1D array containing the normalized frequencies
-    
-    """
-    # Check if the arrays have 2 dimensions
-    if input1.ndim==3 and input2.ndim==3:
-        ns, nr, nc = input1.shape
-    else:
-        raise ValueError("The arrays must have 3 dimensions")
-    # Check if the arrays have the same size
-    if input1.shape != input2.shape:
-        raise ValueError("The arrays must have the same size")
-        
-    # image registration
-    # need to align the two image
-    # input2 = imgregistration(input1,input2,upsamp=100)
-    raise Warning("For now, the alignment of 3D images has not been implemented yet!")
-    
-    
-    # Apodization of the borders    
-    if apod_width == 0:
-        window = 1
-    else:
-        print("Apodization in 3D. This takes time and memory...")
-
-    # sagital slices
-    slicenum = np.round(nr / 2).astype("int")
-    img1_apod = (window * input1)
-    img2_apod = (window * input2)
-    slice1 = img1_apod[:, slicenum, :]
-    slice2 = img2_apod[:, slicenum, :]
-
-    # # plotting
-#     fig1 = plt.figure(1)
-#     ax1 = fig1.add_subplot(121)
-#     ax2 = fig1.add_subplot(122)
-#     im1 = ax1.imshow(slice1, cmap="bone", interpolation="none")
-#     ax1.set_title("image1")
-#     ax1.set_axis_off()
-#     im2 = ax2.imshow(slice2, cmap="bone", interpolation="none")
-#     ax2.set_title("image2")
-#     ax2.set_axis_off()
-    
-    # Setting the threshold
-    if threshold == 'halfbit' or threshold == 'half-bit':
-        print('Computing FSC using half-bit threshold')
-        snrt = 0.2071
-    elif threshold == 'onebit' or threshold =='one-bit':
-        print('Computing FSC using 1-bit threshold')
-        snrt = 0.5
-    else:
-        raise ValueError(
-            "You must choose between 'halfbit' or 'onebit' threshold"
-        )
-
-    # Computation of the FFTs
-    F1 = np.fft.fftn(np.fft.ifftshift(img1_apod)) # FFT of input1
-    F2 = np.fft.fftn(np.fft.ifftshift(img2_apod)) # FFT of input2
-
-    # normalized frequencies
-    f,fnyquist = nyquist((ns, nr,nc)) # Frequencies and Nyquist frequency
-    fn = f/fnyquist
-    
-    # initializing variables
-    C = np.empty_like(f)
-    C1 = np.empty_like(f)
-    C2 = np.empty_like(f)
-    npts = np.zeros_like(f)
-    
-    print("Calculating the correlation...")
-    index = ringthickness(F1) # indexes for the ring thickness
-    for ii in range(len(f)):
-        if ringthick == 0 or ringthick == 1:
-            auxF1 = F1[np.where(index == ii)]
-            auxF2 = F2[np.where(index == ii)]
-        else:
-            auxF1 = F1[
-                (
-                    np.where(
-                        (index >= (ii - ringthick / 2))
-                        & (index <= (ii + ringthick / 2))
-                    )
-                )
-            ]
-            auxF2 = F2[
-                (
-                    np.where(
-                        (index >= (ii - ringthick / 2))
-                        & (index <= (ii + ringthick / 2))
-                    )
-                )
-            ]
-        C[ii] = np.abs((auxF1 * np.conj(auxF2)).sum()) # Cross-correlation
-        C1[ii] = np.abs((auxF1 * np.conj(auxF1)).sum()) # auto-correlation
-        C2[ii] = np.abs((auxF2 * np.conj(auxF2)).sum()) # auto-correlation
-        npts[ii] = auxF1.shape[0]
-
-    # The correlation
-    FSC = C / (np.sqrt(C1 * C2))
-
-    # The computation of the threshold
-    Tnum = (
-        snrt
-        + (2 * np.sqrt(snrt) / np.sqrt(npts))
-        + 1 / np.sqrt(npts)
-    )
-    Tden = (
-        snrt
-        + (2 * np.sqrt(snrt) / np.sqrt(npts))
-        + 1
-    )
-    # The threshold
-    T = Tnum / Tden
-
-    return FSC, T, fn
-    
-
-
-def frc_plot(FRC, T, fn, plot_name):
+def fouriercorr_plot(FRC, T, fn, plot_name):
     """
     Routine to plot the FRC curves
     
