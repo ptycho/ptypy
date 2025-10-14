952ff8f1eb49c237829e9c77cf0b08d045212cf1
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2025-10-14 10:13:26 +0100

    remove pod rescaling and include 3D scan model in other engines

--- ptypy/custom/ML_separate_grads_ptychotomo.py
+++ ptypy/custom/ML_separate_grads_ptychotomo.py
@@ -387,8 +387,6 @@ class MLPtychoTomo(PositionCorrectionEngine):
         when new data arrives.
         """
         self.ML_model.prepare()
-        for name, s in self.pr.storages.items():
-            s.data *= len(s.data)
 
     def get_indexes_of_active_views(self):
         """
